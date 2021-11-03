ebccff33d26b6b2c1b6e15bbcb97bf9cfa347315
Will Cunningham <wjcunningham7@gmail.com>
2021-11-03 10:39:36 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='cova',
+    version=0.1,
+    packages=find_packages(where='.'),
+    maintainer='Agnostiq',
+    author='Agnostiq',
+    install_requires=[],
+    url='https://github.com/AgnostiqHQ/covalent',
+)
