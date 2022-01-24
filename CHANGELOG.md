a5c090f671d9384c85962e2112deb0aaa1107e50
Will Cunningham <wjcunningham7@gmail.com>
2022-01-24 09:40:06 -0500

    adding unit test

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.22.7] - 2022-01-26
+
+### Fixed
+
+- When the local executor's `log_stdout` and `log_stderr` config variables are relative paths, they should go inside the results directory. Previously that was queried from the config, but now it's queried from the lattice metadata.
+
 ## [0.22.6] - 2022-01-26
 
 ### Fixed
---
+++ tests/covalent_tests/customize_executor_test.py
@@ -0,0 +1,67 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+"""Unit test for customizing the local executor."""
+
+import sys
+import tempfile
+from pathlib import Path
+
+import covalent as ct
+
+
+@ct.electron
+def task(x):
+    print(f"stdout: {x}")
+    print("Error!", file=sys.stderr)
+    return x
+
+
+@ct.lattice
+def workflow(x):
+    return task(x)
+
+
+def test_logs_placed_correctly():
+    """Test log files are placed in the correct locations."""
+
+    # Test with absolute paths
+    with tempfile.TemporaryDirectory() as tmpdir:
+        ct.set_config(
+            {
+                "executor.local.log_stdout": f"{tmpdir}/stdout.log",
+                "executor.local.log_stderr": f"{tmpdir}/stderr.log",
+            }
+        )
+
+        workflow.dispatch_sync("absolute")
+
+        # Assert files were created
+        assert Path(f"{tmpdir}/stdout.log").exists()
+        assert Path(f"{tmpdir}/stderr.log").exists()
+
+        # Check file contents
+        with open(f"{tmpdir}/stdout.log") as f:
+            assert len(f.readlines()) == 1
+            assert f.readlines()[0] == "stdout: absolute"
+
+        with open(f"{tmpdir}/stderr.log") as f:
+            assert len(f.readlines()) == 1
+            assert f.readlines()[0] == "Error!"
