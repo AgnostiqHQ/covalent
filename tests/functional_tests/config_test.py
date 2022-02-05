d810e70936d962301a86d6b6d76d0dcf97dfab89
Jalani Kanem <jalani@agnostiq.ai>
2022-02-05 04:46:24 +0000

    Mocked the config file location for configuration unit tests.

--- tests/functional_tests/config_test.py
+++ tests/functional_tests/config_test.py
@@ -19,106 +19,107 @@
 # Relief from the License may be granted by purchasing a commercial license.
 
 import os
-
-import pytest
+from unittest.mock import patch
 
 from covalent._shared_files.config import _ConfigManager
-
-os.environ["COVALENT_CONFIG_DIR"] = "functional_test_files"
 
 
 def test_read_config():
     """Test that configuration file is properly read"""
-    config_manager = _ConfigManager()
-    config_manager.read_config()
 
-    expected_dict = {
-        "sdk": {"log_dir": "./", "log_level": "warning", "enable_logging": "false"},
-        "executors": {
-            "local": {
-                "log_stdout": "stdout.log",
-                "log_stderr": "stderr.log",
-                "cache_dir": "/tmp/covalent",
-                "other_params": {"name": "Local Executor", "proprietary": "False"},
+    with patch.dict(os.environ, {"COVALENT_CONFIG_DIR": "functional_test_files"}, clear=True):
+        config_manager = _ConfigManager()
+        config_manager.read_config()
+
+        expected_dict = {
+            "sdk": {"log_dir": "./", "log_level": "warning", "enable_logging": "false"},
+            "executors": {
+                "local": {
+                    "log_stdout": "stdout.log",
+                    "log_stderr": "stderr.log",
+                    "cache_dir": "/tmp/covalent",
+                    "other_params": {"name": "Local Executor", "proprietary": "False"},
+                },
+                "local_executor": "local.py",
+                "slurm_executor": "slurmp.py",
+                "ssh_executor": "ssh_executor.py",
             },
-            "local_executor": "local.py",
-            "slurm_executor": "slurmp.py",
-            "ssh_executor": "ssh_executor.py",
-        },
-        "test_dict": {
-            "1": 2,
-            "3": 4,
-        },
-    }
+            "test_dict": {
+                "1": 2,
+                "3": 4,
+            },
+        }
 
     assert config_manager.config_data == expected_dict
 
 
 def test_update_config():
     """Test that updating the existing config data with the config file works"""
-    config_manager = _ConfigManager()
-    config_manager.config_data = {
-        "sdk": {
-            "test_dir": "/tmp",
-            "log_dir": {
-                "dir_one": "/var",
-                "dir_two": "~/.log_dir",
-            },
-            "log_level": "debug",
-            "enable_logging": "true",
-        },
-        "executors": {
-            "local": {
-                "other_params": {
-                    "proprietary": "true",
-                    "params_test_dict": {
-                        "a": "b",
-                        "c": "d",
-                    },
-                }
-            },
-            "executor_test_dict": {
-                "alpha": "beta",
-                "gamma": "delta",
-            },
-        },
-        "test_dict": None,
-    }
-    config_manager.update_config()
 
-    expected_dict = {
-        "sdk": {
-            "test_dir": "/tmp",
-            "log_dir": "./",
-            "log_level": "warning",
-            "enable_logging": "false",
-        },
-        "executors": {
-            "local": {
-                "log_stdout": "stdout.log",
-                "log_stderr": "stderr.log",
-                "cache_dir": "/tmp/covalent",
-                "other_params": {
-                    "name": "Local Executor",
-                    "proprietary": "False",
-                    "params_test_dict": {
-                        "a": "b",
-                        "c": "d",
-                    },
+    with patch.dict(os.environ, {"COVALENT_CONFIG_DIR": "functional_test_files"}, clear=True):
+        config_manager = _ConfigManager()
+        config_manager.config_data = {
+            "sdk": {
+                "test_dir": "/tmp",
+                "log_dir": {
+                    "dir_one": "/var",
+                    "dir_two": "~/.log_dir",
+                },
+                "log_level": "debug",
+                "enable_logging": "true",
+            },
+            "executors": {
+                "local": {
+                    "other_params": {
+                        "proprietary": "true",
+                        "params_test_dict": {
+                            "a": "b",
+                            "c": "d",
+                        },
+                    }
+                },
+                "executor_test_dict": {
+                    "alpha": "beta",
+                    "gamma": "delta",
                 },
             },
-            "local_executor": "local.py",
-            "slurm_executor": "slurmp.py",
-            "ssh_executor": "ssh_executor.py",
-            "executor_test_dict": {
-                "alpha": "beta",
-                "gamma": "delta",
+            "test_dict": None,
+        }
+        config_manager.update_config()
+
+        expected_dict = {
+            "sdk": {
+                "test_dir": "/tmp",
+                "log_dir": "./",
+                "log_level": "warning",
+                "enable_logging": "false",
             },
-        },
-        "test_dict": {
-            "1": 2,
-            "3": 4,
-        },
-    }
+            "executors": {
+                "local": {
+                    "log_stdout": "stdout.log",
+                    "log_stderr": "stderr.log",
+                    "cache_dir": "/tmp/covalent",
+                    "other_params": {
+                        "name": "Local Executor",
+                        "proprietary": "False",
+                        "params_test_dict": {
+                            "a": "b",
+                            "c": "d",
+                        },
+                    },
+                },
+                "local_executor": "local.py",
+                "slurm_executor": "slurmp.py",
+                "ssh_executor": "ssh_executor.py",
+                "executor_test_dict": {
+                    "alpha": "beta",
+                    "gamma": "delta",
+                },
+            },
+            "test_dict": {
+                "1": 2,
+                "3": 4,
+            },
+        }
 
     assert config_manager.config_data == expected_dict
