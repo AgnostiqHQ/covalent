347548540da42dd168c677b1982ce0b9d7db90e6
Faiyaz Hasan <faiyaz@agnostiq.ai>
2022-02-11 12:21:46 -0500

    fix test

--- tests/covalent_tests/executor/init_test.py
+++ tests/covalent_tests/executor/init_test.py
@@ -20,12 +20,38 @@
 
 """Tests for Covalent executor init file."""
 
-from covalent.executor import _executor_manager
+from covalent.executor import _executor_manager, _ExecutorManager
 
 
 def test_get_executor_local(mocker):
     """Test that config is reloaded when the get_executor method is called for the local executor."""
 
-    mocked_func = mocker.patch('covalent.executor.reload_config')
+    update_config_mock = mocker.patch("covalent.executor.update_config")
     _executor_manager.get_executor(name="local")
-    mocked_func.assert_called_once_with()
+    update_config_mock.assert_called_once_with()
+
+
+def test_executor_manager_init(mocker):
+    """Test the init method of the executor manager object."""
+
+    generate_plugins_list_mock = mocker.patch(
+        "covalent.executor._ExecutorManager.generate_plugins_list"
+    )
+
+    _ExecutorManager()
+    generate_plugins_list_mock.assert_called_once_with()
+
+
+# def test_executor_manager_generate_plugins_list(mocker):
+#     """Test the generate plugins list method of the executor manager object."""
+
+#     init_mock = mocker.patch("covalent.executor._ExecutorManager.__init__", return_value=None)
+
+#     em = _ExecutorManager()
+#     init_mock.called_once_with()
+
+#     load_executors_mock = mocker.patch("covalent.executor._ExecutorManager._load_executors")
+#     os_path_join = mocker.patch("os.path.join", side_effect=["plugin_path", "default_plugin_path", "user_plugin_path"])
+#     em.generate_plugins_list()
+
+#     assert load_executors_mock.mock_calls == [mocker.call("plugin_path"), mocker.call("default_plugin_path")]
