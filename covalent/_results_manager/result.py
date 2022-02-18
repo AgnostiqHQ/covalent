41d24ed6d8c8f775df15ce198f375041d8f68263
Jalani Kanem <jalani@agnostiq.ai>
2022-02-18 11:13:35 -0500

    Reworded for clarity comments in result.py.

--- covalent/_results_manager/result.py
+++ covalent/_results_manager/result.py
@@ -440,8 +440,7 @@ Node Outputs
         import pkg_resources
 
         dispatch_function = f"# File created by Covalent using covalent version {pkg_resources.get_distribution('cova').version}\n"
-
-        dispatch_function += f"# Result of dispatch {self.dispatch_id}\n"
+        dispatch_function += f"# Results of workflow ID {self.dispatch_id}:\n"
         dispatch_function += f"# Result status: {self.status}\n"
         dispatch_function += f"# Result start time: {self.start_time}\n"
         dispatch_function += f"# Result end time: {self.end_time}" + "\n"
