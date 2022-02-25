47e854a521efa75ef896452d817fa93270fbd67e
Sankalp Sanand <sankalp@agnostiq.ai>
2022-02-25 04:08:44 -0500

    Updated multiproc runner

--- runner/multiproc_runner.py
+++ runner/multiproc_runner.py
@@ -18,22 +18,56 @@
 #
 # Relief from the License may be granted by purchasing a commercial license.
 
-from multiprocessing import Manager
+import os
+from multiprocessing import Pool
 
 from .base_runner import BaseRunner
 
 
 class MultiProcRunner(BaseRunner):
-    def __init__(self, address):
-        self.manager = Manager(
-            address=address,
-        )
+    def __init__(self):
+        self.available_resources = os.cpu_count() - 1
+        self.process_pool = None
 
-    def run_new_task(self, node_id, task_func, task_args, task_kwargs):
+    def begin(self, list_of_tasks):
+        self.process_pool = Pool(processes=self.available_resources)
+
+        for task in list_of_tasks:
+            self.process_pool.apply_async(
+                func=self.run_new_task,
+                args=(task["node_id"], task["func"], task["args"], task["kwargs"]),
+            )
+            self.available_resources -= 1
+
+    @staticmethod
+    def run_new_task(node_id, task_func, task_args, task_kwargs):
+
+        # Start the new task
         pass
 
     def cancel_task(self, node_id):
-        pass
+
+        # Cancel the task
+
+        self.available_resources += 1
 
     def query_task_status(self, node_id):
         pass
+
+    @staticmethod
+    def task_callback(result):
+
+        # Callback once the task is complete
+        pass
+
+    @staticmethod
+    def task_error_callback(resukt):
+
+        # Callback if task failed
+        pass
+
+
+if __name__ == "__main__":
+
+    # Make sure if creating new process, then it should be called within this `if` block.
+    pass
