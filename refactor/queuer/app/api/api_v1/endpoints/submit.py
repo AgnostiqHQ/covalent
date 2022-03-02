38855fcc3e2ac248501df498d186aba116976c38
Alejandro Esquivel <ae@alejandro.ltd>
2022-03-02 15:35:42 -0800

    (wip) Added test publish message to dispatch endpoint

--- refactor/queuer/app/api/api_v1/endpoints/submit.py
+++ refactor/queuer/app/api/api_v1/endpoints/submit.py
@@ -3,18 +3,21 @@ from typing import Any, Optional
 from fastapi import APIRouter, File
 
 from app.schemas.workflow import DispatchResponse
+from app.core.queue import Queuer
 
 router = APIRouter()
 
 
 @router.post("/dispatch", status_code=200, response_model=DispatchResponse)
-def submit_workflow(
+async def submit_workflow(
     *,
     data: bytes
 ) -> Any:
     """
     Submit a workflow
     """
+    queue = Queuer()
+    await queue.publish('foo',{ "message": "hi" })
     return {
         "dispatch_id": "48f1d3b7-27bb-4c5d-97fe-c0c61c197fd5"
     }
