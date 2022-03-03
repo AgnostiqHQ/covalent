d1b7f5949aff2a7544d7893d7640bb77c42af6c9
Sankalp Sanand <sankalp@agnostiq.ai>
2022-03-03 18:58:25 -0500

    some more experimentation, need to send updated node to dispatcher after every completion

--- refactor/runner/core/exp.py
+++ refactor/runner/core/exp.py
@@ -9,20 +9,50 @@ def custom_callback(arg):
     print(f"I'm the callback for {arg}")
 
 
+def send_node_update_to_dispatcher(dispatch_id, node):
+    pass
+
+
 if __name__ == "__main__":
 
     pool = Pool(maxtasksperchild=1)
 
-    args = [[5], [4], [3]]
+    available_tasks = [
+        (task, [6]),
+        (task, [5]),
+        (task, [4]),
+        (task, [3]),
+        (task, [2]),
+        (task, [1]),
+    ]
+    available_resources = 4
+    execution_results = []
 
-    async_results = [pool.apply_async(task, args=arg, callback=custom_callback) for arg in args]
+    async_results = []
 
-    i = 0
-    while True:
-        if i >= len(async_results):
-            i = 0
+    # for ar in async_results:
+    #     print(ar.get())
+    while available_tasks:
 
-        elif async_results[i].ready():
-            break
+        for i in range(available_resources):
+            func, args = available_tasks[i]
 
-        i += 1
+            async_results.append(pool.apply_async(func, args=args, callback=custom_callback))
+
+            available_resources -= 1
+            available_tasks.pop(i)
+
+        i = 0
+        while True:
+            if i >= len(async_results):
+                i = 0
+
+            elif async_results[i].ready():
+                available_resources += 1
+                break
+
+            i += 1
+
+        send_node_update_to_dispatcher("dispatch_id", async_results[i].get())
+
+        del async_results[i]
