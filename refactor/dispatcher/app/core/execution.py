c6d335639f4394e270e4cd535b04d46e635a4708
Faiyaz Hasan <faiyaz@agnostiq.ai>
2022-03-04 16:07:41 -0500

    update refactor

--- refactor/dispatcher/app/core/execution.py
+++ refactor/dispatcher/app/core/execution.py
@@ -30,8 +30,12 @@ Dummy methods for calls to the Runner API.
 """
 
 
-def run_task(dispatch_id: int, task_id: int) -> bool:
-    """Ask Runner to execute task - get back True (False) if resources are (not) available."""
+def run_task(result_object: Result, task_id_batch: List[int]) -> bool:
+    """Ask Runner to execute tasks - get back True (False) if resources are (not) available.
+
+    The Runner might not have resources available to pick up the batch of tasks. In that case,
+    this function continues to try running the tasks until the runner becomes free.
+    """
 
     pass
 
@@ -51,6 +55,11 @@ def update_ui(result_object: Result):
     pass
 
 
+"""
+Status checking methods.
+"""
+
+
 def is_workflow_running(result_object: Result) -> bool:
     """Check if workflow is running."""
 
@@ -60,8 +69,25 @@ def is_workflow_running(result_object: Result) -> bool:
 def is_workflow_completed(result_object: Result) -> bool:
     """Check if workflow is completed"""
 
-    # TODO - Clarify if FAILED status belongs here
-    return result_object == "COMPLETED" or "FAILED"
+    if result_object == Result.COMPLETED:
+        return True
+    elif result_object == Result.FAILED:
+        return True
+    else:
+        task_id_batches = result_object.lattice.transport_graph.get_topologically_sorted_graph()
+
+        for task_batch in task_id_batches:
+            for node_id in task_batch:
+                if (
+                    result_object._get_node_status(node_id) == Result.FAILED
+                    or Result.RUNNING
+                    or Result.CANCELLED
+                ):
+                    return False
+
+    # TODO - Ideally this setting of the status should happen elsewhere
+    result_object.status = Result.COMPLETED
+    return True
 
 
 def is_task_completed(task_id: int, result_object: Result) -> bool:
@@ -70,6 +96,11 @@ def is_task_completed(task_id: int, result_object: Result) -> bool:
     # TODO - Refactor `node_id` -> `task_id`
     # TODO - Clarify if FAILED status belongs here
     return result_object.get_node_status(task_id=task_id) == "COMPLETED" or "FAILED"
+
+
+"""
+Dispatcher functions.
+"""
 
 
 def cancel_workflow():
@@ -83,6 +114,13 @@ def update_task_results(task_execution_result: Dict, result_object: Result):
 
     result_object._update_node(**task_execution_result)
 
+    if is_workflow_completed(result_object=result_object):
+        update_workflow_results(result_object=result_object)
+
+    # TODO - Double check that this is fine. It's place holder for writing the result to the
+    #  database.
+    result_object.save()
+
 
 def _post_process(lattice: Lattice, node_outputs: Dict, task_exec_order: List[List]):
     """TODO - Describe post processing here."""
@@ -92,6 +130,8 @@ def _post_process(lattice: Lattice, node_outputs: Dict, task_exec_order: List[Li
 
 def update_workflow_results(workflow_execution_result: Dict, result_object: Result):
     """Update workflow results"""
+
+    # TODO - The logic to find the workflow_execution_result might need to go in here
 
     # TODO - This might be a very central component; consider in more detail
     order = result_object.lattice.transport_graph.get_topologically_sorted_graph()
@@ -113,7 +153,13 @@ def run_workflow():
 
 
 def dispatch_workflow(result_object: Result) -> None:
-    """If workflow is not running, i.e. just picked up from the queue, run dispatcher."""
+    """After a workflow has been popped from a queue (remember that dispatcher processes only
+    one workflow at a time) and it has been validated, this function is run.
+
+    If workflow is not running, i.e. just picked up from the queue, run dispatcher.
+    """
+
+    # This is the beginning of dispatch: result.status = Result.NEW_OBJ
 
     # TODO - Consider having a result_object.copy_transport_graph() method.
     # TODO - Consider creating a method to carry out the functionality below.
@@ -126,4 +172,13 @@ def dispatch_workflow(result_object: Result) -> None:
     # writing the result object update to the database. We might not want to constantly read and
     # write.
 
-    run_workflow()
+    # Beginning the workflow execution process.
+    result_object.status = Result.RUNNING
+    scheduled_tasks = result_object.lattice.transport_graph.get_topologically_sorted_graph()
+
+    # TODO - Modify this logic so that it applies to sub-lattices as well
+    while scheduled_tasks:
+        run_task(result_object=result_object, task_id_batch=scheduled_tasks.pop(0))
+
+    # After all the scheduled tasks have successfully been sent to the Runner API and the while
+    # loop above has completed, it doesn't mean that the workflow execution status is COMPLETED.
