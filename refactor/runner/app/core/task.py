adf7a0db76891b39cd20812f12478f797c8b74d0
Sankalp Sanand <sankalp@agnostiq.ai>
2022-03-10 09:05:03 -0500

    updated endpoints with commented out functions, yet to test with dispatcher

--- refactor/runner/app/core/task.py
+++ refactor/runner/app/core/task.py
@@ -21,7 +21,7 @@
 
 from multiprocessing import Queue as MPQ
 
-from .exp import get_task_status, run_available_tasks
+from .execution import get_task_status, run_available_tasks
 
 cancelled_tasks_queue = MPQ()
 track_status_queue = MPQ()
