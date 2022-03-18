e8053c71b459c107125bb15d0984b72fdaabab4f
Faiyaz Hasan <faiyaz@agnostiq.ai>
2022-03-18 11:03:53 -0400

    update queue consumption example

--- refactor/dispatcher/scripts/consumer_example.py
+++ refactor/dispatcher/scripts/consumer_example.py
@@ -10,6 +10,18 @@ load_dotenv()
 TOPIC = os.environ.get("MQ_DISPATCH_TOPIC")
 MQ_CONNECTION_URI = os.environ.get("MQ_CONNECTION_URI")
 
+
+async def main():
+    print("Connecting to NATS...")
+    nc = await nats.connect(MQ_CONNECTION_URI)
+
+    async def msg_handler(msg):
+        subject = msg.subject
+        reply = msg.reply
+        data = msg.data.decode()
+        print(f"Recieved a msg on '{subject} {reply}': {data}")
+
+
 #
 # if __name__ == "__main__":
 #     loop = asyncio.get_event_loop()
