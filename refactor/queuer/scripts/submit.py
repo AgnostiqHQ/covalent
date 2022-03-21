899251490ae2cca7c049340119278fb4b2a0cdc0
Alejandro Esquivel <ae@alejandro.ltd>
2022-03-21 15:22:01 -0700

    Removed testing scripts

--- refactor/queuer/scripts/submit.py
+++
@@ -1,9 +0,0 @@
-import requests
-import os
-
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, './result.pkl')
-
-with open(filename, 'rb') as f:   
-    res = requests.post(url='http://localhost:8000/api/v0/submit/dispatch', files={'result_pkl_file': f})
-    print(res.text)
