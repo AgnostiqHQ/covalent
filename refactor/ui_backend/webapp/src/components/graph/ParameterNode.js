8322797d8c58e228cec881374a331773ed08208b
Alejandro Esquivel <ae@alejandro.ltd>
2022-03-24 15:28:12 -0700

    Moved webapp to refactor ui_backend folder

---
+++ refactor/ui_backend/webapp/src/components/graph/ParameterNode.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2021 Agnostiq Inc.
+ *
+ * This file is part of Covalent.
+ *
+ * Licensed under the GNU Affero General Public License 3.0 (the "License").
+ * A copy of the License may be obtained with this software package or at
+ *
+ *      https://www.gnu.org/licenses/agpl-3.0.en.html
+ *
+ * Use of this file is prohibited except in compliance with the License. Any
+ * modifications or derivative works of this file must retain this copyright
+ * notice, and modified files must contain a notice indicating that they have
+ * been altered from the originals.
+ *
+ * Covalent is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+ *
+ * Relief from the License may be granted by purchasing a commercial license.
+ */
+
+import { Typography } from '@mui/material'
+import { Handle } from 'react-flow-renderer'
+
+const ParameterNode = ({
+  id,
+  data,
+  type,
+  selected,
+  sourcePosition,
+  targetPosition,
+  isConnectable,
+}) => {
+  return (
+    <Typography
+      component="div"
+      sx={(theme) => ({
+        px: 2,
+        py: 0.5,
+        borderRadius: 3,
+        color: 'text.disabled',
+        bgcolor: theme.palette.background.paper,
+        fontSize: 12,
+      })}
+    >
+      {data.label}
+      <Handle
+        type="source"
+        position={sourcePosition}
+        isConnectable={isConnectable}
+      />
+    </Typography>
+  )
+}
+
+export default ParameterNode
