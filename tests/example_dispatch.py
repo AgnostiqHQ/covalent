8ba5c0008503adba8c144c19dadb64bc195567c7
Okechukwu Ochia <emmirald@gmail.com>
2022-03-25 14:22:52 -0600

    Adding path to covalent

--- tests/example_dispatch.py
+++ tests/example_dispatch.py
@@ -21,11 +21,15 @@
 """
 An example script containing a simple workflow that can be dispatched to Covalent
 """
-
+import sys
+import os
 import time
+from requests import request
+sys.path.append(os.path.abspath(os.path.join(os.path.pardir, '../covalent')))
+
 import covalent as ct
 import covalent_dispatcher._cli.service as service
-from requests import request
+
 
 
 @ct.electron
