415602c2b2ac588bf9a687b2a9970dd23b4b9b57
Sankalp Sanand <sankalp@agnostiq.ai>
2022-03-25 02:13:43 -0400

    Modified port numbers until I merge supervisord

--- refactor/dispatcher/testing.py
+++ refactor/dispatcher/testing.py
@@ -1,7 +1,7 @@
 import requests
 
 dispatch_id = "pog_dispatch_id"
-url = f"http://localhost:8000/api/v0/workflow/{dispatch_id}"
+url = f"http://localhost:8003/api/v0/workflow/{dispatch_id}"
 
 resp = requests.post(url=url)
 resp.raise_for_status()
--- refactor/runner/app/core/execution.py
+++ refactor/runner/app/core/execution.py
@@ -61,7 +61,7 @@ def generate_task_result(
 
 def send_task_update_to_dispatcher(dispatch_id, task_result):
 
-    url = f"http://localhost:8000/api/v0/workflow/{dispatch_id}"
+    url = f"http://localhost:8003/api/v0/workflow/{dispatch_id}"
     requests.put(url=url, data=pickle.dumps(task_result))
 
     logger.warning(f"{dispatch_id}")
@@ -70,13 +70,13 @@ def send_task_update_to_dispatcher(dispatch_id, task_result):
 
 def free_resources_call_to_runner(dispatch_id, task_id):
 
-    url = f"http://localhost:8000/api/v0/workflow/{dispatch_id}/task/{task_id}/free"
+    url = f"http://localhost:8004/api/v0/workflow/{dispatch_id}/task/{task_id}/free"
     requests.post(url=url)
 
 
 def done_callback_to_runner(dispatch_id, task_id):
 
-    url = f"http://localhost:8000/api/v0/workflow/{dispatch_id}/task/{task_id}/done"
+    url = f"http://localhost:8004/api/v0/workflow/{dispatch_id}/task/{task_id}/done"
     requests.post(url=url)
 
 
