f71b6e80e4aa080bec3c9c0d4b2005a453255615
Scott Wyman Neagle <scott@agnostiq.ai>
2022-03-31 17:25:31 -0400

    test download endpoint #146

--- refactor/data/tests/main_test.py
+++ refactor/data/tests/main_test.py
@@ -3,23 +3,40 @@ import os
 
 from app.core.localstoragebackend import LocalStorageBackend
 
-dirname = os.path.dirname(__file__)
-filename = os.path.join(dirname, "./_test_assets/result")
+DIRNAME = os.path.dirname(__file__)
+FILENAME = os.path.join(DIRNAME, "./_test_assets/result")
 
 
 def test_upload_endpoint(test_app, monkeypatch):
-    MOCK_OBJECT_NAME = "mockresult"
+    mock_object_name = "mockresult"
 
     def mock_put(_, data, bucket_name, object_name, length, metadata=None):
         return (bucket_name, object_name)
 
     monkeypatch.setattr("app.core.localstoragebackend.LocalStorageBackend.put", mock_put)
 
-    with open(filename, "rb") as f:
+    with open(FILENAME, "rb") as f:
         response = test_app.post(
             "/api/v0/fs/upload",
-            files=[("file", (MOCK_OBJECT_NAME, f, "application/octet-stream"))],
+            files=[("file", (mock_object_name, f, "application/octet-stream"))],
         )
         d = response.json()
-        assert d["filename"] == MOCK_OBJECT_NAME
+        assert d["filename"] == mock_object_name
         assert d["path"] == "default"
+
+
+def test_download_endpoint(test_app, monkeypatch):
+    mock_object_name = "mockresult"
+
+    def mock_file_reader(filename):
+        with open(filename, "rb") as f:
+            yield from f
+
+    def mock_get(_, bucket_name, object_name):
+        return mock_file_reader(FILENAME)
+
+    monkeypatch.setattr("app.core.localstoragebackend.LocalStorageBackend.get", mock_get)
+
+    response = test_app.get("/api/v0/fs/download", params={"file_location": FILENAME}, stream=True)
+    assert response.status_code < 400
+    assert len(response.content) > 0
