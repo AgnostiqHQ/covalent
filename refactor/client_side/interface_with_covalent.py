f8d8fa338fcb8c42b766574a1f07c6b3d92fc92a
Sankalp Sanand <sankalp@agnostiq.ai>
2022-03-31 21:24:01 -0400

    wrote the updated dispatch function

--- refactor/client_side/interface_with_covalent.py
+++ refactor/client_side/interface_with_covalent.py
@@ -22,3 +22,42 @@
 """Functions to dispatch a workflow and get result of a workflow"""
 
 # Include a download to file parameter to allow user to save the result in a file if required
+
+from copy import deepcopy
+from functools import wraps
+from io import BytesIO
+
+import cloudpickle as pickle
+import requests
+
+from covalent._results_manager.result import Result
+from covalent._workflow.lattice import Lattice
+
+QUEUER_ADDR = "http://localhost:8000"
+
+
+def dispatch(
+    orig_lattice: Lattice,
+):
+    @wraps(orig_lattice)
+    def wrapper(*args, **kwargs) -> str:
+
+        lattice = deepcopy(orig_lattice)
+
+        lattice.build_graph(*args, **kwargs)
+
+        # Serializing the transport graph and then passing it to the Result object
+        lattice.transport_graph = lattice.transport_graph.serialize()
+
+        pickled_res = pickle.dumps(Result(lattice, lattice.metadata["results_dir"]))
+        test_url = f"{QUEUER_ADDR}/api/v0/submit/dispatch"
+
+        r = requests.post(test_url, files={"result_pkl_file": BytesIO(pickled_res)})
+        r.raise_for_status()
+        return r.content.decode("utf-8").strip().replace('"', "")
+
+    return wrapper
+
+
+def get_result():
+    pass
