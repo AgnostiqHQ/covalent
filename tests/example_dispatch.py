13483ce255c0783b2fc79c0fcdb1a6bc2912aef5
Scott Wyman Neagle <scott@agnostiq.ai>
2022-04-05 16:22:47 -0400

    Updating the actions to push microservice images to public repos.
    This commit also contains lots of style changes due to `pre-commit run
    --all-files`.
    
    I can split those changes into another PR if it's too difficult to merge
    them now.
    
    AgnostiqHQ/covalent-infra#18

--- tests/example_dispatch.py
+++ tests/example_dispatch.py
@@ -22,18 +22,17 @@
 An example script containing a simple workflow that can be dispatched to Covalent.
 
 """
-import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.pardir, '../covalent/covalent')))
+import sys
 import time
 
 from requests import request
 
 import covalent as ct
-from requests import request
 import covalent_dispatcher._cli.service as service
 
 port = 48008
+sys.path.append(os.path.abspath(os.path.join(os.path.pardir, "../covalent/covalent")))
 
 
 @ct.electron
