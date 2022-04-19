6e867d90be88ac610bc54e0cde93840ab8a6e0c5
Sankalp Sanand <sankalp@agnostiq.ai>
2022-04-19 02:22:06 -0400

    slightly reverting the change to result's test

--- covalent_results/tests/main_test.py
+++ covalent_results/tests/main_test.py
@@ -92,7 +92,7 @@ def test_put(test_app, monkeypatch):
     monkeypatch.setattr("app.core.api.DataService.download", mock_download)
     monkeypatch.setattr("app.core.api.DataService.upload", mock_upload)
     monkeypatch.setattr("app.core.db.Database.value", mock_value)
-    task = {"node_id": 0, "output": 27}
+    task = {"node_id": 0, "node_name": "subtask", "output": 27}
 
     response = test_app.put(
         f"/api/v0/workflow/results/{MOCK_DISPATCH_ID}", files={"task": BytesIO(pickle.dumps(task))}
