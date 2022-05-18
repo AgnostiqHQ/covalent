bd861ebd1c3df4c1fc1781a682bc8a9c874843d8
Faiyaz Hasan <faiyaz@agnostiq.ai>
2022-05-18 16:26:35 -0400

    remove outdated queue test

--- covalent_queuer/tests/app/core/queuer_test.py
+++ covalent_queuer/tests/app/core/queuer_test.py
@@ -1,22 +1,9 @@
 import os
 
 import mock
-import nats
 import pytest
-import requests
 from app.core.queue import Queue
 
 
 class TestQueue:
-
-    MOCK_MQ_CONNECTION_URI = "localhost:4222"
-
-    @pytest.mark.parametrize(
-        reason="No longer supporting the NATS message queue. This test needs to be updated."
-    )
-    @mock.patch.object(nats, "connect", autospec=True)
-    @mock.patch.dict(os.environ, {"MQ_CONNECTION_URI": MOCK_MQ_CONNECTION_URI})
-    def test_get_client(self, mocked_nats):
-        queue = Queue()
-        queue.get_client()
-        mocked_nats.assert_called_once_with(self.MOCK_MQ_CONNECTION_URI)
+    pass
