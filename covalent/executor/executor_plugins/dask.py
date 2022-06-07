f838d35b5bec19d2ce4174c105869c55d2bd9d4a
Venkat Bala <venkat@agnostiq.ai>
2022-06-07 15:33:46 -0400

    Simpler dask startup (#609)
    * Default config settings for Dask
    
    * Updated required packages to include dask-distributed
    
    * Updated CLI tool to manage dask clusters
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Fixes
    
    * Fixing scheduler address
    
    * Fixing scheduler address
    
    * Added app for starting a dask cluster
    
    * Added dask executor
    
    * Setting dask as default executor
    
    * Added dask to cli
    
    * Removing dask distributed requirement
    
    * Removed dask from executor plugins
    
    * Adding dask executor
    
    * Updated log level
    
    * Added dask requirement
    
    * Added check for dask process liveness
    
    * start dask cluster before main covalent launch in a different process
    
    * make signals more readable
    
    * create a dask cluster process by extending python multiprocessing
    
    * start dask cluster before main covalent launch in a different process
    
    * make signals more readable
    
    * create a dask cluster process by extending python multiprocessing
    
    * rebased on 504-dask-startup branch
    
    * add bokeh to requirements.txt
    
    * update changelog
    
    fix pre-commit issues
    
    fix pre-commit issues
    
    pre-commit ci fix
    
    pre-commit ci hook fix
    
    * freeze version of furo to prevent breaking doc builds (#610)
    
    * freeze version of furo to prevent breaking doc builds
    
    * update/fix changelog
    
    * The new version will be 0.106.2
    
    * Use pull_request_target for changelog_reminder workflow. (#606)
    
    * Use pull_request_target for changelog_reminder workflow.
    
    We don't build anything so this doesn't introduce any security
    vulnerabilities.
    
    * Update changelog
    
    Co-authored-by: Scott Wyman Neagle <scott@agnostiq.ai>
    
    * changelog update
    
    Co-authored-by: Scott Wyman Neagle <scott@agnostiq.ai>
    
    * skip testing stdout/stderr redirection until implemented
    
    * update changelog
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix precommit
    
    * remove splitting string to get scheduler_port
    
    * remove comments
    
    * remove unused dask import
    
    * update changelog
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: Okechukwu Ochia <emmirald@gmail.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: CovalentOpsBot <covalentopsbot@users.noreply.github.com>
    Co-authored-by: Casey Jao <casey@agnostiq.ai>
    Co-authored-by: Scott Wyman Neagle <scott@agnostiq.ai>
    Co-authored-by: Venkat Bala <venkat@agnositq.ai>

---
+++ covalent/executor/executor_plugins/dask.py
@@ -0,0 +1,145 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+"""
+Module for defining a Dask executor that submits the input python function in a dask cluster
+and waits for execution to finish then returns the result.
+
+This is a plugin executor module; it is loaded if found and properly structured.
+"""
+
+import io
+import os
+from contextlib import redirect_stderr, redirect_stdout
+from typing import Any, Dict, List
+
+from dask.distributed import get_client
+
+from covalent._shared_files import logger
+
+# Relative imports are not allowed in executor plugins
+from covalent._shared_files.config import get_config
+from covalent._shared_files.util_classes import DispatchInfo
+from covalent._workflow.transport import TransportableObject
+from covalent.executor import BaseExecutor
+
+# The plugin class name must be given by the executor_plugin_name attribute:
+executor_plugin_name = "DaskExecutor"
+
+app_log = logger.app_log
+log_stack_info = logger.log_stack_info
+
+_EXECUTOR_PLUGIN_DEFAULTS = {
+    "log_stdout": "stdout.log",
+    "log_stderr": "stderr.log",
+    "cache_dir": os.path.join(
+        os.environ.get("XDG_CACHE_HOME") or os.path.join(os.environ["HOME"], ".cache"), "covalent"
+    ),
+}
+
+
+class DaskExecutor(BaseExecutor):
+    """
+    Dask executor class that submits the input function to a running dask cluster.
+    """
+
+    def __init__(
+        self,
+        scheduler_address: str = "",
+        log_stdout: str = "stdout.log",
+        log_stderr: str = "stderr.log",
+        conda_env: str = "",
+        cache_dir: str = "",
+        current_env_on_conda_fail: bool = False,
+    ) -> None:
+        if not cache_dir:
+            cache_dir = os.path.join(
+                os.environ.get("XDG_CACHE_HOME") or os.path.join(os.environ["HOME"], ".cache"),
+                "covalent",
+            )
+
+        if scheduler_address == "":
+            scheduler_address = get_config("dask.scheduler_address")
+
+        super().__init__(log_stdout, log_stderr, conda_env, cache_dir, current_env_on_conda_fail)
+
+        self.scheduler_address = scheduler_address
+
+    def execute(
+        self,
+        function: TransportableObject,
+        args: List,
+        kwargs: Dict,
+        dispatch_id: str,
+        results_dir: str,
+        node_id: int = -1,
+    ) -> Any:
+        """
+        Executes the input function and returns the result.
+
+        Args:
+            function: The input python function which will be executed and whose result
+                      is ultimately returned by this function.
+            args: List of positional arguments to be used by the function.
+            kwargs: Dictionary of keyword arguments to be used by the function.
+            dispatch_id: The unique identifier of the external lattice process which is
+                         calling this function.
+            results_dir: The location of the results directory.
+            node_id: The node ID of this task in the bigger workflow graph.
+
+        Returns:
+            output: The result of the executed function.
+        """
+
+        dask_client = get_client(address=self.scheduler_address, timeout=1)
+
+        dispatch_info = DispatchInfo(dispatch_id)
+        fn = function.get_deserialized()
+        fn_version = function.python_version
+
+        with self.get_dispatch_context(dispatch_info), redirect_stdout(
+            io.StringIO()
+        ) as stdout, redirect_stderr(io.StringIO()) as stderr:
+
+            if self.conda_env != "":
+                result = None
+
+                result = self.execute_in_conda_env(
+                    fn,
+                    fn_version,
+                    args,
+                    kwargs,
+                    self.conda_env,
+                    self.cache_dir,
+                    node_id,
+                )
+
+            else:
+                future = dask_client.submit(fn, *args, **kwargs)
+                result = future.result()
+
+        self.write_streams_to_file(
+            (stdout.getvalue(), stderr.getvalue()),
+            (self.log_stdout, self.log_stderr),
+            dispatch_id,
+            results_dir,
+        )
+
+        return (result, stdout.getvalue(), stderr.getvalue())
