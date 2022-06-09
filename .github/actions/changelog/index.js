8cfb9db73af4166abd3aec7e6d948ca61fd7187b
Scott Wyman Neagle <scott@agnostiq.ai>
2022-06-09 14:36:28 -0400

    trim whitespace after parsing version

--- .github/actions/changelog/index.js
+++ .github/actions/changelog/index.js
@@ -3,7 +3,9 @@ const fs = require("fs");
 const readline = require("readline");
 
 try {
-  const head_version = fs.readFileSync(core.getInput("version-path"), "utf8");
+  const head_version = fs
+    .readFileSync(core.getInput("version-path"), "utf8")
+    .trim();
   const changelog = fs.readFileSync(core.getInput("changelog-path"), "utf8");
   let curline = 0;
   const begin = 8;
@@ -14,35 +16,28 @@ try {
   let patch = false;
   let minor = false;
   let noupdate = false;
-  console.log('head_version is ' + head_version)
-  console.log('Node version is: ' + process.version);
   rl.on("line", (text) => {
-    console.log('begin ' + begin)
-    console.log('end ' + end)
-    console.log('parsing line ' + curline)
-    console.log(text.match(head_version))
     if (curline < begin || curline > end) {
       curline++;
       return;
     }
-   if (text.match(head_version)) {
-      console.log('updating end line')
+    if (text.match("\\b" + head_version + "\\b")) {
       end = curline++;
       rl.close();
       return;
     }
-    console.log(text)
+    console.log(text);
     if (
       text.includes("### Added") ||
       text.includes("### Changed") ||
       text.includes("### Removed")
     ) {
       minor = true;
-      console.log('found minor version update')
+      console.log("found minor version update");
     }
     if (text.includes("### Fixed")) {
       patch = true;
-      console.log('found patch version update')
+      console.log("found patch version update");
     }
     if (
       text.includes("### Tests") ||
@@ -50,49 +45,55 @@ try {
       text.includes("### Operations")
     ) {
       noupdate = true;
-      console.log('found noop header')
-      // comment to trigger workflow run
+      console.log("found noop header");
     }
     curline++;
   });
-  const semver = head_version.split(".");
-  let vmajor = semver[0];
-  let vminor = semver[1];
-  let vpatch = semver[2];
-  if (minor) {
-    vminor++;
-  } else if (patch) {
-    vpatch++;
-  } else if (noupdate) {
-    //do nothing
-  } else {
-    core.setFailed(
-      "Changelog does not contain enough information to update the version."
-    );
-  }
-  const version = vmajor + "." + vminor + "." + vpatch;
-  const changelog_header =
-    "## [" + version + "] - " + new Date().toISOString().split("T")[0];
-  let message = "noop";
-  if (minor || patch) {
-    message = "The new version will be " + version;
-    unreleased = "UNRELEASED";
-    const new_changelog = changelog
-      .slice(0, changelog.indexOf(unreleased) + unreleased.length + 1)
-      .concat(
-        "\n",
-        "\n",
-        changelog_header,
-        changelog.slice(changelog.indexOf(unreleased) + unreleased.length + 1)
+  rl.on("close", () => {
+    console.log("minor is " + minor);
+    console.log("patch is " + patch);
+    console.log("noupdate is " + noupdate);
+    const semver = head_version.split(".");
+    let vmajor = semver[0];
+    let vminor = semver[1];
+    let vpatch = semver[2];
+    if (minor) {
+      vminor++;
+    } else if (patch) {
+      vpatch++;
+    } else if (noupdate) {
+      //do nothing
+    } else {
+      core.setFailed(
+        "Changelog does not contain enough information to update the version."
       );
-    fs.writeFileSync(core.getInput("changelog-path", new_changelog, "utf8"));
-    fs.writeFileSync(core.getInput("version-path", version, "utf8"));
-  } else {
-    console.log(
-      "This PR only contains updates to tests and docs. No release will be created."
-    );
-  }
-  core.setOutput("message", message);
+    }
+    const version = vmajor + "." + vminor + "." + vpatch;
+    const changelog_header =
+      "## [" + version + "] - " + new Date().toISOString().split("T")[0];
+    let message = "noop";
+    if (minor || patch) {
+      message = "The new version will be " + version;
+      unreleased = "UNRELEASED";
+      const new_changelog = changelog
+        .slice(0, changelog.indexOf(unreleased) + unreleased.length + 1)
+        .concat(
+          "\n",
+          "\n",
+          changelog_header,
+          changelog.slice(changelog.indexOf(unreleased) + unreleased.length + 1)
+        );
+      fs.writeFileSync(core.getInput("changelog-path", new_changelog, "utf8"));
+      fs.writeFileSync(core.getInput("version-path", version, "utf8"));
+    } else if (noupdate) {
+      console.log(
+        "This PR only contains updates to tests and docs. No release will be created."
+      );
+    } else {
+      console.log("No release will be created due to a failure.");
+    }
+    core.setOutput("message", message);
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
