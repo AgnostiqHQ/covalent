250c8f4cb7a2d4de4520b951a37f13570192c15f
WingCode <smallstar1234@gmail.com>
2022-06-17 17:37:35 +0530

    Add test for sort edges based on insertion order.

--- tests/covalent_tests/workflow/transport_test.py
+++ tests/covalent_tests/workflow/transport_test.py
@@ -255,3 +255,32 @@ def test_transport_graph_deserialize(workflow_transport_graph):
 
     transportable_obj = wtg.get_node_value(node_key=0, value_key="function")
     assert transportable_obj.get_deserialized()(x=3) == 9
+
+
+def test_transport_graph_sort_edges_based_on_insertion_order(transport_graph):
+    """Test the transport graph sorting edges based on insertion order"""
+    tg = transport_graph
+    assert len(tg._graph.nodes) == 0
+    tg.add_node(
+        name="triangle",
+        kwargs={"x": 2},
+        function=subtask,
+        metadata={"mock_field": "mock_value_node_0"},
+    )
+    tg.add_node(
+        name="square",
+        kwargs={"x": 2},
+        function=subtask,
+        metadata={"mock_field": "mock_value_node_1"},
+    )
+    tg.add_node(
+        name="rectangle",
+        kwargs={"x": 2},
+        function=subtask,
+        metadata={"mock_field": "mock_value_node_2"},
+    )
+    tg.add_edge(x=2, y=1, edge_name="apples")
+    tg.add_edge(x=0, y=1, edge_name="oranges")
+
+    tg.sort_edges_based_on_insertion_order()
+    assert list(tg.get_dependencies(node_key=1)) == [2, 0]
