c282ef4deeb9a5f2f8a8b6ff09911b1567f2f886
Casey Jao <casey@agnostiq.ai>
2022-06-24 13:22:43 -0400

    Add DepsBash test

--- tests/covalent_tests/workflow/deps_tests.py
+++ tests/covalent_tests/workflow/deps_tests.py
@@ -1,6 +1,12 @@
 import covalent as ct
 
 
+def test_deps_bash_init():
+    cmds = ["pip list", "yum install gcc"]
+    deps = ct.DepsBash(cmds)
+    assert deps.commands == cmds
+
+
 def test_bash_deps_apply():
     cmds = ["pip list", "yum install gcc"]
     deps = ct.DepsBash(cmds)
