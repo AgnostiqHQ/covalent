4f7d4a7e00add04f7d3388b1827d25591259cbcc
ArunPsiog <arun.mukesh@psiog.com>
2022-06-24 18:22:40 +0530

    Sample commit to verify access

--- covalent_ui/app.py
+++ covalent_ui/app.py
@@ -11,7 +11,6 @@
 # modifications or derivative works of this file must retain this copyright
 # notice, and modified files must contain a notice indicating that they have
 # been altered from the originals.
-#
 # Covalent is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 # FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
