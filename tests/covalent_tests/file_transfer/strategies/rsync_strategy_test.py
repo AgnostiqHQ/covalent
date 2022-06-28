dde415fa59f72cd4edc8cb1e05345b6f14cbeb60
Alejandro Esquivel <ae@alejandro.ltd>
2022-06-27 23:26:45 -0700

    removed harcoded fixtures

--- tests/covalent_tests/file_transfer/strategies/rsync_strategy_test.py
+++ tests/covalent_tests/file_transfer/strategies/rsync_strategy_test.py
@@ -5,19 +5,23 @@ from covalent._file_transfer.strategies.rsync_strategy import Rsync
 
 
 class TestRsyncStrategy:
+    MOCK_HOST = "11.11.11.11"
+    MOCK_USER = "admin"
+    MOCK_REMOTE_FILEPATH = "/home/ubuntu/observations.csv"
+
     def test_get_rsync_cmd(self):
-        file = File(remote_path="/home/ubuntu/observations.csv")
-        upload_cmd = Rsync(user="admin", host="11.11.11.11").get_rsync_cmd(
+        file = File(remote_path=self.MOCK_REMOTE_FILEPATH)
+        upload_cmd = Rsync(user=self.MOCK_USER, host=self.MOCK_HOST).get_rsync_cmd(
             file, transfer_from_remote=False
         )
-        download_cmd = Rsync(user="admin", host="11.11.11.11").get_rsync_cmd(
+        download_cmd = Rsync(user=self.MOCK_USER, host=self.MOCK_HOST).get_rsync_cmd(
             file, transfer_from_remote=True
         )
         assert (
             upload_cmd
-            == f"rsync -e ssh /tmp/{file.id} admin@11.11.11.11:/home/ubuntu/observations.csv"
+            == f"rsync -e ssh /tmp/{file.id} {self.MOCK_USER}@{self.MOCK_HOST}:{self.MOCK_REMOTE_FILEPATH}"
         )
         assert (
             download_cmd
-            == f"rsync -e ssh admin@11.11.11.11:/home/ubuntu/observations.csv /tmp/{file.id}"
+            == f"rsync -e ssh {self.MOCK_USER}@{self.MOCK_HOST}:{self.MOCK_REMOTE_FILEPATH} /tmp/{file.id}"
         )
