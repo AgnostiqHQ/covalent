db9cd9bd52f2e66cc00af5bfa081f32c04a0bf8f
Alejandro Esquivel <ae@alejandro.ltd>
2022-07-06 09:49:49 -0700

    Update covalent/_file_transfer/folder.py
    Co-authored-by: Scott Wyman Neagle <scott@agnostiq.ai>

--- covalent/_file_transfer/folder.py
+++ covalent/_file_transfer/folder.py
@@ -5,8 +5,8 @@ from covalent._file_transfer.file import File
 
 class Folder(File):
     """
-    Folder class to store components of provided URI including scheme (s3://, file://, ect.) determine if the file is remote,
-    and acts a facade to facilitate filesystem operations. Is a child of the File class which sets is_dir flag to True.
+    Folder class to store components of provided URI including scheme (s3://, file://, ect.), determine if the file is remote,
+    and act as facade to facilitate filesystem operations. Folder is a child of the File class which sets `is_dir` flag to True.
 
     Attributes:
         include_folder: Flag that determines if the folder should be included in the file transfer, if False only contents of folder are transfered.
