bded1d2dc2f2bf4da1b9d0921ffc321f0918685d
Okechukwu Ochia <emmirald@gmail.com>
2022-07-09 09:41:23 -0600

    Added test for file transfer

--- tests/covalent_tests/workflow/lepton_test.py
+++ tests/covalent_tests/workflow/lepton_test.py
@@ -1,3 +1,4 @@
+import inspect
 import os
 import pytest
 
@@ -5,6 +6,7 @@ from subprocess import Popen, PIPE
 from ctypes import c_int32, POINTER
 from tempfile import NamedTemporaryFile
 from covalent._workflow.lepton import Lepton
+from covalent._file_transfer.file_transfer import File, FileTransfer, Order, HTTP
 
 test_py_module = """
 def entrypoint(x: int, y: int) -> int:
@@ -53,14 +55,14 @@ def test_init_lepton():
     assert mock_py_lepton.argtypes == []
 
     mock_c_lepton = Lepton(
-                language="C",
-                library_name=c_source_path, function_name="test_entry",
-                argtypes=[
-                    (c_int32, Lepton.INPUT),
-                    (POINTER(c_int32), Lepton.INPUT_OUTPUT),
-                    (POINTER(c_int32), Lepton.OUTPUT)
-                    ],
-                )
+        language="C",
+        library_name=c_source_path, function_name="test_entry",
+        argtypes=[
+            (c_int32, Lepton.INPUT),
+            (POINTER(c_int32), Lepton.INPUT_OUTPUT),
+            (POINTER(c_int32), Lepton.OUTPUT)
+        ],
+    )
 
     assert mock_c_lepton.language == "C"
     assert mock_c_lepton.library_name == temp_c_source_file.name
@@ -70,7 +72,9 @@ def test_init_lepton():
 
 def test_lepton_languages(
         supported_languages=("C", "c", "python", "Python"),
-        unsupported_languages=("R", "r", "Erlang", "erlang", "Haskell", "haskell")
+        unsupported_languages=("R", "r", "Erlang", "erlang", "Haskell", "haskell", "Julia", "julia", "C++", "c++",
+                               "Fortan", "fortran"
+                               )
 ):
     for language in supported_languages:
         assert language in Lepton._LANG_C or Lepton._LANG_PY
@@ -80,10 +84,76 @@ def test_lepton_languages(
             Lepton(language=language)
 
 
-def test_file_transfer_support():
+@pytest.mark.parametrize(
+    "is_from_file_remote, is_to_file_remote", [(False, False)]
+)
+@pytest.mark.parametrize("order", (Order.BEFORE, Order.AFTER))
+def test_local_file_transfer_support(is_from_file_remote, is_to_file_remote, order):
     """
-    Test file transfer in a lepton
+    Test for local file transfer
     """
+
+    mock_from_file = File("file:///home/source.csv", is_remote=is_from_file_remote)
+    mock_to_file = File("file:///home/dest.csv", is_remote=is_to_file_remote)
+
+    mock_file_transfer = FileTransfer(from_file=mock_from_file, to_file=mock_to_file, order=order)
+    mock_lepton_with_files = Lepton(files=[mock_file_transfer])
+    call_before_deps = mock_lepton_with_files.get_metadata('call_before')
+    call_after_deps = mock_lepton_with_files.get_metadata('call_after')
+    print(mock_file_transfer.strategy)
+    if mock_file_transfer.order == "before":
+        # No file transfer should be done after executing the lepton
+        assert call_after_deps == []
+
+    if mock_file_transfer.order == "after":
+        # No file transfer should be done before executing the lepton
+        assert call_before_deps == []
+
+    # The defined file transfer strategies should not be mutated by the lepton
+    for call in call_before_deps:
+        assert inspect.getsource(call.apply_fn.get_deserialized()) == inspect.getsource(mock_file_transfer.cp())
+    for call in call_after_deps:
+        assert inspect.getsource(call.apply_fn.get_deserialized()) == inspect.getsource(mock_file_transfer.cp())
+
+
+@pytest.mark.parametrize(
+    "is_from_file_remote, is_to_file_remote",
+    [
+        (False, True),
+        (True, False),
+        (True, True)
+    ]
+)
+@pytest.mark.parametrize("order", (Order.BEFORE, Order.AFTER))
+def test_http_file_transfer(is_from_file_remote, is_to_file_remote, order):
+    """
+    Test for http file transfer
+    """
+    mock_from_file = File("http://example.com/source.csv")
+    mock_to_file = File("file:///home/dest.csv")
+    mock_file_download = FileTransfer(from_file=mock_from_file, to_file=mock_to_file, order=order)
+    mock_lepton_with_files = Lepton(files=[mock_file_download])
+
+    # Test HTTP Upload strategy is not currently supported
+    with pytest.raises(NotImplementedError):
+        Lepton(files=[FileTransfer(from_file=mock_to_file, to_file=mock_from_file, strategy=HTTP())])
+
+    call_before_deps = mock_lepton_with_files.get_metadata('call_before')
+    call_after_deps = mock_lepton_with_files.get_metadata('call_after')
+
+    if mock_file_download.order == "before":
+        # No file transfer should be done after executing the lepton
+        assert call_after_deps == []
+
+    if mock_file_download.order == "after":
+        # No file transfer should be done before executing the lepton
+        assert call_before_deps == []
+
+    # The defined file transfer strategies should not be mutated by the lepton
+    for call in call_before_deps:
+        assert inspect.getsource(call.apply_fn.get_deserialized()) == inspect.getsource(mock_file_download.cp())
+    for call in call_after_deps:
+        assert inspect.getsource(call.apply_fn.get_deserialized()) == inspect.getsource(mock_file_download.cp())
 
 
 def test_python_wrapper():
