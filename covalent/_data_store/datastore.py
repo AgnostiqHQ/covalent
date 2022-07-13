92714dd3783a69ec57d013fc0e01958e99440cf9
Alejandro Esquivel <ae@alejandro.ltd>
2022-07-13 15:54:27 -0700

    Added programatic methods of fetching head & current revision

--- covalent/_data_store/datastore.py
+++ covalent/_data_store/datastore.py
@@ -21,10 +21,16 @@
 from contextlib import contextmanager
 from os import path
 from pathlib import Path
-from typing import BinaryIO, Dict, Generator, List, Optional, Union
+from typing import BinaryIO, Dict, List, Optional, Union
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
+
+from alembic import command
+from alembic.config import Config
+from alembic.environment import EnvironmentContext
+from alembic.migration import MigrationContext
+from alembic.script import ScriptDirectory
 
 from .._shared_files.config import get_config
 from . import models
@@ -54,15 +60,30 @@ class WorkflowDB:
         if initialize_db:
             models.Base.metadata.create_all(self.engine)
 
-    def run_migrations(self):
-        from alembic import command
-        from alembic.config import Config
-
+    def get_alembic_config(self, logging_enabled: bool = True):
         alembic_ini_path = Path(path.join(__file__, "./../../../alembic.ini")).resolve()
         alembic_config = Config(alembic_ini_path)
-        alembic_config.attributes["configure_logger"] = False
+        alembic_config.attributes["configure_logger"] = logging_enabled
         alembic_config.set_main_option("sqlalchemy.url", self.db_URL)
+        return alembic_config
+
+    def run_migrations(self, logging_enabled: bool = True):
+        alembic_config = self.get_alembic_config(logging_enabled=logging_enabled)
         command.upgrade(alembic_config, "head")
+
+    def current_revision(self):
+        alembic_config = self.get_alembic_config(logging_enabled=False)
+        script = ScriptDirectory.from_config(alembic_config)
+        with EnvironmentContext(alembic_config, script) as ctx:
+            context = MigrationContext.configure(self.engine.connect(), environment_context=ctx)
+            current_rev = context.get_current_revision()
+            return current_rev
+
+    def current_head(self):
+        alembic_config = self.get_alembic_config(logging_enabled=False)
+        script = ScriptDirectory.from_config(alembic_config)
+        current_head = script.get_current_head()
+        return current_head
 
     @contextmanager
     def begin_session(self, metadata={}):
