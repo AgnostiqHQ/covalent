8950e396891bb1da9f8e790db8bc64d275cd968d
Poojith <poojith@agnostiq.ai>
2022-07-25 11:28:54 +0530

    add-s3_strategy-for-file-transfer

---
+++ covalent/_file_transfer/strategies/s3_strategy.py
@@ -0,0 +1,68 @@
+import os
+import urllib.request
+from pathlib import Path
+from subprocess import PIPE, CalledProcessError, Popen
+from typing import Optional
+
+from covalent._file_transfer import File
+from covalent._file_transfer.strategies.transfer_strategy_base import FileTransferStrategy
+
+import boto3
+from furl import furl
+
+class S3(FileTransferStrategy):
+
+    """
+    Implements Base FileTransferStrategy class to use HTTP to download files from public URLs.
+    """
+
+    def __init__(self,
+                 aws_access_key_id : str = None,
+                 aws_secret_access_key : str = None,
+                 aws_session_token : str = None,
+                 region_name : str = None):
+
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+        self.aws_session_token = aws_session_token
+        self.region_name = region_name
+        
+        
+    # return callable to download here implies 'from' is a remote source
+    def download(self, from_file: File, to_file: File = File()) -> File:
+        from_filepath = from_file.filepath
+        to_filepath = to_file.filepath
+        bucket_name = furl(from_file.uri).origin[5:] 
+
+        def callable():
+            s3 = boto3.client("s3",
+                              aws_access_key_id = self.aws_access_key_id,
+                              aws_secret_access_key = self.aws_secret_access_key,
+                              aws_session_token = self.aws_session_token,
+                              region_name = self.region_name,
+            )
+            s3.download_file(bucket_name, from_filepath, to_filepath)
+
+        return callable
+
+    # return callable to download here implies 'to' is a remote source
+    def upload(self, from_file: File, to_file: File = File()) -> File:
+
+        from_filepath = from_file.filepath
+        to_filepath = to_file.filepath
+        bucket_name = furl(to_file.uri).origin[5:] 
+
+        def callable():
+            s3 = boto3.client("s3",
+                              aws_access_key_id = self.aws_access_key_id,
+                              aws_secret_access_key = self.aws_secret_access_key,
+                              aws_session_token = self.aws_session_token,
+                              region_name = self.region_name,
+            )
+            s3.upload_file(from_filepath,bucket_name, to_filepath)
+
+        return callable
+    
+    # No S3 Strategy for copy
+    def cp(self, from_file: File, to_file: File = File()) -> File:
+        raise NotImplementedError
---
+++ tests/covalent_tests/file_transfer/strategies/s3_strategy_test.py
@@ -0,0 +1,44 @@
+import pytest
+
+from covalent._file_transfer import File
+from covalent._file_transfer.strategies.s3_strategy import S3
+
+from furl import furl
+
+class TestS3Strategy:
+
+    MOCK_LOCAL_FILEPATH = "/Users/user/data.csv"
+    MOCK_REMOTE_FILEPATH = "s3://covalent-tmp/data.csv"
+
+    def test_download(self, mocker):
+        # validate urlretrieve called with appropriate arguments
+        boto3_mock = mocker.patch("boto3.client")
+        boto3_mock.download_file.return_value = None
+        
+        from_file = File(self.MOCK_REMOTE_FILEPATH)
+        to_file = File(self.MOCK_LOCAL_FILEPATH)
+
+        bucket_name = furl(from_file.uri).origin[5:]
+        
+        S3().download(from_file, to_file)()
+        
+        boto3_mock().download_file.assert_called_with(bucket_name, from_file.filepath, to_file.filepath)
+
+    def test_upload(self, mocker):
+        # validate urlretrieve called with appropriate arguments
+        boto3_mock = mocker.patch("boto3.client")
+        boto3_mock.upload_file.return_value = None
+        
+        to_file = File(self.MOCK_REMOTE_FILEPATH)
+        from_file = File(self.MOCK_LOCAL_FILEPATH)
+
+        bucket_name = furl(to_file.uri).origin[5:]
+        
+        S3().upload(from_file, to_file)()
+        
+        boto3_mock().upload_file.assert_called_with(from_file.filepath,bucket_name, to_file.filepath)
+    
+
+    def test_cp_failure(self, mocker):
+        with pytest.raises(NotImplementedError):
+            S3().cp(File(self.MOCK_REMOTE_FILEPATH), File(self.MOCK_LOCAL_FILEPATH))()
