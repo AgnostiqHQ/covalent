2879d953c42fa85f655d476d8600dfd994810890
Amalan Jenicious F <amalan.jenicious@psiog.com>
2022-08-30 11:06:03 +0530

    Test cases for lattice distpatch and controls

--- covalent_ui/webapp/src/components/graph/__tests__/LatticeControls.test.js
+++ covalent_ui/webapp/src/components/graph/__tests__/LatticeControls.test.js
@@ -20,22 +20,9 @@
 //  * Relief from the License may be granted by purchasing a commercial license.
 //  */
 
-import { render, screen, fireEvent, waitFor } from '@testing-library/react'
+import { render, screen, fireEvent, waitFor, cleanup } from '@testing-library/react'
 import App from '../LatticeControls'
 import "@testing-library/jest-dom";
-import { ToggleButton, ToggleButtonGroup, Tooltip } from '@mui/material'
-import {
-  Add as PlusIcon,
-  ArrowBack,
-  ArrowDownward,
-  ArrowForward,
-  ArrowUpward,
-  Fullscreen,
-  LockOpenOutlined,
-  LockOutlined,
-  MapOutlined,
-  Remove as MinusIcon,
-} from '@mui/icons-material'
 import React from 'react'
 import { Provider } from 'react-redux'
 import reducers from '../../../redux/reducers'
@@ -43,11 +30,11 @@ import { configureStore } from '@reduxjs/toolkit'
 import theme from '../../../utils/theme'
 import ThemeProvider from '@mui/system/ThemeProvider'
 import { BrowserRouter } from 'react-router-dom'
-
 import { ReactFlowProvider } from 'react-flow-renderer'
 import { HelmetProvider } from 'react-helmet-async'
 
-const onSubmit = jest.fn();
+import { act, renderHook } from '@testing-library/react-hooks'
+import userEvent from "@testing-library/user-event";
 
 function reduxRender(renderedComponent) {
   const store = configureStore({
@@ -86,7 +73,6 @@ describe('lattice toggle buttons', () => {
   })
 
   test('renders fit to screen button', async () => {
-    // const handleClick = jest.fn()
     reduxRender(<App openDialogBox={true} title={'Fit to screen'} />)
     const linkElement = screen.getByTestId('FullscreenIcon')
     fireEvent.click(screen.getByTestId('FullscreenIcon'))
@@ -130,4 +116,63 @@ describe('lattice toggle buttons', () => {
     expect(screen.getByLabelText('Toggle parameters')).toBeInTheDocument('Toggle parameters')
     expect(linkElement).toBeVisible();
   })
+
+  const direction = ["TB"]
+  const setDirection = ['BT']
+
+  const direction1 = ["BT"]
+  const setDirection1 = ['LR']
+
+  const direction2 = ["LR"]
+  const setDirection2 = ['RL']
+
+  const direction3 = ["RL"]
+  const setDirection3 = ['TB']
+
+  afterEach(cleanup)
+
+  test('should render downward icon', () => {
+    const container = reduxRender(<App direction={direction} setDirection={setDirection} />);
+    const { result } = renderHook(() => <App direction={direction} setDirection={setDirection} />)
+    const linkElement = screen.getByLabelText("Change orientation");
+    userEvent.click(linkElement)
+    act(() => { result.current.props })
+    expect(result.current.props.direction).toBe(direction)
+    expect(result.current.props.setDirection).toBe(setDirection)
+    expect(screen.getByTestId("ArrowDownwardIcon")).toBeInTheDocument()
+  })
+
+  test('should render upward icon', () => {
+    const container = reduxRender(<App direction={direction1} setDirection={setDirection1} />);
+    const { result } = renderHook(() => <App direction={direction1} setDirection={setDirection1} />)
+    const linkElement = screen.getByLabelText("Change orientation");
+    userEvent.click(linkElement)
+    act(() => { result.current.props })
+    expect(result.current.props.direction).toBe(direction1)
+    expect(result.current.props.setDirection).toBe(setDirection1)
+    expect(screen.getByTestId("ArrowUpwardIcon")).toBeInTheDocument()
+  })
+
+  test('should render back icon', () => {
+    const container = reduxRender(<App direction={direction2} setDirection={setDirection2} />);
+    const { result } = renderHook(() => <App direction={direction2} setDirection={setDirection2} />)
+    const linkElement = screen.getByLabelText("Change orientation");
+    userEvent.click(linkElement)
+    act(() => { result.current.props })
+    expect(result.current.props.direction).toBe(direction2)
+    expect(result.current.props.setDirection).toBe(setDirection2)
+    expect(screen.getByTestId("ArrowForwardIcon")).toBeInTheDocument()
+  })
+
+  test('should render forward icon', () => {
+    const container = reduxRender(<App direction={direction3} setDirection={setDirection3} />);
+    const { result } = renderHook(() => <App direction={direction3} setDirection={setDirection3} />)
+    const linkElement = screen.getByLabelText("Change orientation");
+    userEvent.click(linkElement)
+    act(() => { result.current.props })
+    expect(result.current.props.direction).toBe(direction3)
+    expect(result.current.props.setDirection).toBe(setDirection3)
+    expect(screen.getByTestId("ArrowBackIcon")).toBeInTheDocument()
+  })
+
 })
