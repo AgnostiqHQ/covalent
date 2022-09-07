6f94f4fa8719e545db84482986bed73baef03005
Okechukwu  Emmanuel Ochia <okechukwu@agnostiq.ai>
2022-09-07 04:23:57 -0600

    Update remote_executor.py

--- covalent/executor/executor_plugins/remote_executor.py
+++ covalent/executor/executor_plugins/remote_executor.py
@@ -121,6 +121,7 @@ class RemoteExecutor(AsyncBaseExecutor):
         """
         Abstract method that retrieves the pickled result from the remote cache.
         """
+        pass
 
     @abstractmethod
     async def cancel(self) -> bool:
