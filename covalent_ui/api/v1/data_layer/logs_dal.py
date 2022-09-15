feae9ab862417edd15dfc58ad2d50f1f8070ef99
Manjunath PV <manjunath.poilath@psiog.com>
2022-09-15 14:37:19 +0530

    #1185 - Fix for logs sorting

--- covalent_ui/api/v1/data_layer/logs_dal.py
+++ covalent_ui/api/v1/data_layer/logs_dal.py
@@ -19,7 +19,7 @@
 # Relief from the License may be granted by purchasing a commercial license.
 
 import re
-from operator import itemgetter
+from datetime import datetime
 
 from covalent._shared_files.config import get_config
 from covalent_ui.api.v1.models.logs_model import LogsResponse
@@ -30,6 +30,21 @@ class Logs:
 
     def __init__(self) -> None:
         self.config = get_config
+
+    def __split_merge_line(self, output_arr: list, split_reg, line, last_msg=""):
+        match = re.match(split_reg, line)
+        if match:
+            if last_msg == "":
+                output_arr.append(line)
+            else:
+                if len(output_arr) == 0:
+                    output_arr.append(last_msg)
+                output_arr.append(line)
+                output_arr[len(output_arr) - 1] += last_msg
+                last_msg = ""
+        else:
+            last_msg += "\n" + line
+        return last_msg
 
     def get_logs(self, sort_by, direction, search, count, offset):
         """
@@ -43,55 +58,56 @@ class Logs:
         Return:
             List of top most Lattices and count
         """
-        output_data = []
-        result_data = []
+        output_data, result_data = [], []
+        file_name, file_location = "covalent_ui.log", self.config("user_interface.log_dir")
         last_msg = ""
+        reverse_list = bool(direction.value == "DESC")
 
-        reverse_list = True if direction.value == "DESC" else False
-
-        split_line = (
+        split_line, split_words = (
             r"\[[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]{1,3})?,[0-9]+]"
-        )
-        file_name = "covalent_ui.log"
-        file_location = self.config("user_interface.log_dir")
-        with open(f"{file_location}/{file_name}", "r") as logfile:
-            for line in logfile:
-                match = re.match(split_line, line)
-                if match:
-                    if last_msg == "":
-                        output_data.append(line)
-                    else:
-                        if len(output_data) == 0:
-                            output_data.append(line)
-                        output_data[len(output_data) - 1] += last_msg
-                        last_msg = ""
+        ), (r"\[(.*)\] \[(TRACE|DEBUG|INFO|NOTICE|WARN|WARNING|ERROR|SEVERE|FATAL)\] ((.|\n)*)")
+
+        try:
+            with open(f"{file_location}/{file_name}", "r") as logfile:
+                for line in logfile:
+                    msg = self.__split_merge_line(output_data, split_line, line, last_msg)
+                    last_msg = msg
+                if last_msg != "":
+                    if len(output_data) == 0:
+                        output_data.append(last_msg)
+                    output_data[len(output_data) - 1] += last_msg
+                    last_msg = ""
+        except FileNotFoundError:
+            output_data = []
+
+        if len(output_data) >= 1:
+            regex_word = re.compile(split_words)
+            for line in output_data:
+                reg = regex_word.split(line.rstrip("\n"))
+                if len(reg) >= 3:
+                    parse_str = datetime.strptime(reg[1], "%Y-%m-%d %I:%M:%S,%f")
+                    json_data = {"log_date": f"{parse_str}", "status": reg[2], "message": reg[3]}
+                    result_data.append(json_data)
                 else:
-                    last_msg += "\n" + line
-            if last_msg != "":
-                output_data[len(output_data) - 1] += last_msg
-                last_msg = ""
+                    result_data.append({"log_date": None, "status": "INFO", "message": reg[0]})
 
-        split_words = (
-            r"\[(.*)\] \[(TRACE|DEBUG|INFO|NOTICE|WARN|WARNING|ERROR|SEVERE|FATAL)\] ((.|\n)*)"
-        )
-        regex_word = re.compile(split_words)
+            modified_data = sorted(
+                result_data,
+                key=lambda e: (e[sort_by.value] is None, e[sort_by.value]),
+                reverse=reverse_list,
+            )
 
-        for line in output_data:
-            reg = regex_word.split(line.rstrip("\n"))
-            if len(reg) >= 3:
-                json_data = {"log_date": reg[1], "status": reg[2], "message": reg[3]}
-                result_data.append(json_data)
-            else:
-                result_data.append({"log_date": None, "status": "INFO", "message": reg[0]})
+            if search != "":
+                modified_data = [
+                    x
+                    for x in modified_data
+                    if (search.lower() in x["message"].lower())
+                    or (search.lower() in x["status"].lower())
+                ]
 
-        modified_data = sorted(result_data, key=itemgetter(sort_by.value), reverse=reverse_list)
-
-        if search != "":
-            modified_data = [
-                x for x in modified_data if (search in x["message"]) or (search in x["status"])
-            ]
-
-        modified_data = modified_data[offset:]
-        modified_data = modified_data[:count]
+            modified_data = modified_data[offset:]
+            modified_data = modified_data[:count]
+        else:
+            modified_data = []
 
         return LogsResponse(items=modified_data, total_count=len(result_data))
