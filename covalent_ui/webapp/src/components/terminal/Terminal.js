7d304d24e459c98181e94e9b9f2ef487e767aa55
Prasy12 <prasanna.venkatesh@psiog.com>
2022-09-15 18:26:34 +0530

    Terminal UI changes

--- covalent_ui/webapp/src/components/terminal/Terminal.js
+++ covalent_ui/webapp/src/components/terminal/Terminal.js
@@ -5,53 +5,52 @@ import { XTerm } from 'xterm-for-react'
 import { FitAddon } from 'xterm-addon-fit';
 import { WebLinksAddon } from 'xterm-addon-web-links';
 import socket from '../../utils/socket'
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Chip } from '@mui/material';
 import Typography from '@mui/material/Typography';
 
 const Terminal = () => {
     const xtermRef = useRef()
     const webLinksAddon = new WebLinksAddon()
     const fitAddon = new FitAddon()
-    const [status, setStatus] = useState('Disconnected')
     useEffect(() => {
-        console.log(status)
-    },[status])
-    useEffect(() => {
-        socket.on('disconnect', () => {
-            setStatus('Disconnected')
+        socket.emit('start_terminal', () => {
+            console.debug(`socket ${socket.id} connected: ${socket.connected}`)
         })
+        return () => {
+            socket.emit('stop_terminal')
+        }
     })
-
-    useEffect(() => {
-        xtermRef.current.terminal.writeln(" Please press Enter to connect to the server terminal");
-    }, [])
     useEffect(() => {
         socket.on("pty-output", function (data) {
             xtermRef.current.terminal.write(data.output);
         });
         return () => {
             socket.off('pty-output')
-          }
+        }
     })
     return (
-        <Container maxWidth="xl" sx={{ mb: 4, mt: 7.5, ml: 4 }}>
-            <Typography variant="h4" component="h4">
-                Terminal
-            </Typography>
-            <Grid container spacing={3} sx={{ mt: 4 }}>
-                <XTerm
-                    options={{
-                        cursorBlink: true,
-                        macOptionIsMeta: true,
-                        scrollback: true,
-                    }}
-                    ref={xtermRef}
-                    onData={(data) => {
-                        socket.emit("pty-input", { input: data })
-                    }}
-                    addons={[fitAddon, webLinksAddon]} />
+        <Container sx={{ mb: 4, mt: 7.5, ml: 5 }}>
+            <Grid xs={12} sx={{ mb: 4 }}>
+                <Typography sx={{ fontSize: '2rem' }} component="h4" display="inline">
+                    Terminal
+                </Typography>
+                <Chip sx={{ height: '24px',ml:1,mb:1.5, fontSize: '0.75rem',color:'#FFFFFF' }} label='BETA' variant='outlined' />
             </Grid>
-
+            <XTerm
+                options={{
+                    cursorBlink: true,
+                    macOptionIsMeta: true,
+                    scrollback: true,
+                    theme: {
+                        background: '#08081A'
+                    },
+                    cols: 8
+                }}
+                ref={xtermRef}
+                onData={(data) => {
+                    socket.emit("pty-input", { input: data })
+                }}
+                addons={[fitAddon, webLinksAddon]} />
         </Container>
 
     )
