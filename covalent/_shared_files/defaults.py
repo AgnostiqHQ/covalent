3577500ccb7cd78f202c53e74baa21de7bbe259d
Sankalp Sanand <sankalp@agnostiq.ai>
2022-09-19 23:09:38 -0400

    Fix for default executor value setting

--- covalent/_shared_files/defaults.py
+++ covalent/_shared_files/defaults.py
@@ -144,7 +144,7 @@ def get_default_executor() -> dict:
     """
     from .config import get_config
 
-    return "local" if get_config(CMType.CLIENT, "sdk.no_cluster") else "dask"
+    return "local" if get_config(CMType.CLIENT, "sdk.no_cluster") == "true" else "dask"
 
 
 def get_default_config_path(type: CMType) -> str:
