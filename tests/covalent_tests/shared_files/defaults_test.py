3e829dce5df36ad9b2cd4c9e4a1cd62df436d8ca
Sankalp Sanand <sankalp@agnostiq.ai>
2022-09-20 00:48:08 -0400

    Added all unit tests for defaults.py

--- tests/covalent_tests/shared_files/defaults_test.py
+++ tests/covalent_tests/shared_files/defaults_test.py
@@ -23,6 +23,7 @@
 import os
 from unittest import mock
 
+import dask.system as dask_system
 import pytest
 
 
@@ -33,10 +34,15 @@ def env_vars():
         "LOGLEVEL": "TEST_LOG_LEVEL",
         "COVALENT_LOG_TO_FILE": "test_log",
         "COVALENT_EXECUTOR_DIR": "test_exec_dir",
+        "COVALENT_DISPATCHER_ADDR": "test_dispatcher_addr",
+        "COVALENT_SVC_PORT": "0000",
+        "COVALENT_SERVER_IFACE_ANY": "test_server_iface",
+        "COVALENT_DEV_PORT": "1111",
+        "COVALENT_RESULTS_DIR": "test_results_dir",
     }
 
 
-def test_default_sdk_config(env_vars):
+def test_get_default_sdk_config(env_vars):
     """Tests that the default SDK configuration is as expected"""
 
     with mock.patch.dict(os.environ, env_vars, clear=True):
@@ -50,3 +56,88 @@ def test_default_sdk_config(env_vars):
         assert received_config["enable_logging"] == "test_log"
         assert received_config["executor_dir"] == "test_exec_dir"
         assert received_config["no_cluster"] == "false"
+
+
+def test_get_default_server_config(env_vars):
+    """Tests that the default server configuration is as expected"""
+
+    with mock.patch.dict(os.environ, env_vars, clear=True):
+        from covalent._shared_files.defaults import get_default_server_config
+
+        received_config = get_default_server_config()
+
+        assert received_config["address"] == "test_dispatcher_addr"
+        assert received_config["port"] == 0000
+
+
+def test_get_default_service_config(env_vars):
+    """Tests that the default service configuration is as expected"""
+
+    with mock.patch.dict(os.environ, env_vars, clear=True):
+        from covalent._shared_files.defaults import get_default_service_config
+
+        received_config = get_default_service_config()
+
+        assert received_config["address"] == "0.0.0.0"
+        assert received_config["port"] == 0000
+        assert received_config["dev_port"] == 1111
+        assert received_config["results_dir"] == "test_results_dir"
+        assert received_config["cache_dir"] == "test_home/.cache/covalent"
+        assert received_config["log_dir"] == "test_home/.cache/covalent"
+        assert received_config["db_path"] == "test_home/.local/share/covalent/dispatcher_db.sqlite"
+
+
+def test_get_default_dask_config(env_vars):
+    """Tests that the default dask configuration is as expected"""
+
+    with mock.patch.dict(os.environ, env_vars, clear=True):
+        from covalent._shared_files.defaults import get_default_dask_config
+
+        received_config = get_default_dask_config()
+
+        assert received_config["cache_dir"] == "test_home/.cache/covalent"
+        assert received_config["log_dir"] == "test_home/.cache/covalent"
+        assert received_config["mem_per_worker"] == "auto"
+        assert received_config["threads_per_worker"] == 1
+        assert received_config["num_workers"] == dask_system.CPU_COUNT
+
+
+def test_get_default_workflow_data_config(env_vars):
+    """Tests that the default workflow data configuration is as expected"""
+
+    with mock.patch.dict(os.environ, env_vars, clear=True):
+        from covalent._shared_files.defaults import get_default_workflow_data_config
+
+        received_config = get_default_workflow_data_config()
+
+        assert received_config["storage_type"] == "local"
+        assert received_config["base_dir"] == "test_home/.local/share/covalent/workflow_data"
+
+
+def test_get_default_executor():
+    """Tests whether the default executor is set as expected"""
+
+    with mock.patch("covalent._shared_files.config.get_config", return_value="true"):
+        from covalent._shared_files.defaults import get_default_executor
+
+        assert get_default_executor() == "local"
+
+    with mock.patch("covalent._shared_files.config.get_config", return_value="false"):
+        from covalent._shared_files.defaults import get_default_executor
+
+        assert get_default_executor() == "dask"
+
+
+def test_get_default_config_path(env_vars):
+    """
+    Tests that the default configuration path is as expected
+    depending on whether the client or the server is accessing it
+    """
+
+    with mock.patch.dict(os.environ, env_vars, clear=True):
+        from covalent._shared_files.defaults import CMType, get_default_config_path
+
+        assert get_default_config_path(CMType.CLIENT) == "test_home/.config/covalent/covalent.conf"
+        assert (
+            get_default_config_path(CMType.SERVER) == "test_home/.config/covalent/covalentd.conf"
+        )
