41da30aa963d7468e710aea49b62f2dd33424075
Prasanna Venkatesh <54540812+Prasy12@users.noreply.github.com>
2022-09-22 12:12:22 +0530

    UI logs feature (#1264)
    * #1185 - Implemented logs API
    
    * #1185 - Fix for logs sorting
    
    * #1185 - list sorting null values fix
    
    * Added logs page
    
    * #1185 - Logs download fix
    
    * Logs page UI change
    
    * Adding new electron statuses
    
    * Electron status changes
    
    * #1185 - logs count fix
    
    * Changes for model helper
    
    * Changes on request and response model
    
    * Added homepage resolution for 1500px
    
    * Logs table UI changes
    
    * Download functionality changes for logs
    
    * #1185 - offset and count conflict issue fix
    
    * Introduce pagination in logs
    
    * Added zoom in and out icons in lattice controls
    
    * #1185 - config changes for log API
    
    * Changed logs table height
    
    * #1185 - Added critical log type
    
    * Add navbar to dispatch not found view.
    
    * Nav bar changes.
    
    * Font size changed and status text color added
    
    * Changes to download on the UI.
    
    * Disable download button when download is in progress.
    
    * Changed logs table height and preview width
    
    * Change cancelled status label color.
    
    * Changes for logs
    
    * #1185 - search logs bug fix
    
    * Graph UI changes
    
    * Fix parameter node position
    
    * Altered the unit tests
    
    * Update changelog for logs
    
    * #1185 - date format bug fix
    
    Co-authored-by: Manjunath PV <manjunath.poilath@psiog.com>
    Co-authored-by: kamalesh.suresh <kamalesh.suresh@psiog.com>
    Co-authored-by: ArunPsiog <arun.mukesh@psiog.com>
    Co-authored-by: RaviPsiog <raviteja.gurram@psiog.com>

--- covalent_ui/webapp/src/components/dispatch/_tests_/DispatchDrawerContents.test.js
+++ covalent_ui/webapp/src/components/dispatch/_tests_/DispatchDrawerContents.test.js
@@ -20,88 +20,90 @@
  * Relief from the License may be granted by purchasing a commercial license.
  */
 
- import { screen } from '@testing-library/react'
- import App from '../DispatchDrawerContents'
- import { BrowserRouter } from 'react-router-dom'
- import React from 'react'
- import { Provider } from 'react-redux'
- import { render } from '@testing-library/react'
- import reducers from '../../../redux/reducers'
- import { configureStore } from '@reduxjs/toolkit'
- import theme from '../../../utils/theme'
- import ThemeProvider from '@mui/system/ThemeProvider'
+import { screen, render } from '@testing-library/react'
+import App from '../DispatchDrawerContents'
+import { BrowserRouter } from 'react-router-dom'
+import React from 'react'
+import { Provider } from 'react-redux'
+import reducers from '../../../redux/reducers'
+import { configureStore } from '@reduxjs/toolkit'
+import theme from '../../../utils/theme'
+import ThemeProvider from '@mui/system/ThemeProvider'
 
- function reduxRender(renderedComponent) {
-   const store = configureStore({
-     reducer: reducers,
-   })
+function reduxRender(renderedComponent) {
+  const store = configureStore({
+    reducer: reducers,
+  })
 
-   return render(
-     <Provider store={store}>
-       <ThemeProvider theme={theme}>
-         <BrowserRouter>{renderedComponent}</BrowserRouter>
-       </ThemeProvider>
-     </Provider>
-   )
- }
+  return render(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>{renderedComponent}</BrowserRouter>
+      </ThemeProvider>
+    </Provider>
+  )
+}
 
- describe('Dispatch drawer contents', () => {
+describe('Dispatch drawer contents', () => {
   const drawerLatticeDetails = {
-    directory: "/home/prasannavenkatesh/Desktop/workflows/results/4be2f8f9-528d-4006-9e13-0ddf84172c98",
-    dispatch_id: "4be2f8f9-528d-4006-9e13-0ddf84172c98",
-    ended_at: "2022-08-09T11:49:33",
+    directory:
+      '/home/prasannavenkatesh/Desktop/workflows/results/4be2f8f9-528d-4006-9e13-0ddf84172c98',
+    dispatch_id: '4be2f8f9-528d-4006-9e13-0ddf84172c98',
+    ended_at: '2022-08-09T11:49:33',
     runtime: 2000,
-    started_at: "2022-08-09T11:49:31",
-    status: "COMPLETED",
+    started_at: '2022-08-09T11:49:31',
+    status: 'COMPLETED',
     total_electrons: 54,
     total_electrons_completed: 54,
     updated_at: null,
   }
 
-   test('Dispatch drawer contents is rendered', () => {
-     reduxRender(<App />)
-     const linkElement = screen.getByTestId('latticedispatchoverview')
-     expect(linkElement).toBeInTheDocument()
-   })
+  test('Dispatch drawer contents is rendered', () => {
+    reduxRender(<App />)
+    const linkElement = screen.getByTestId('latticedispatchoverview')
+    expect(linkElement).toBeInTheDocument()
+  })
 
   test('drawerLatticeDetailsFetching is rendered', () => {
-    reduxRender(<App drawerLatticeDetailsFetching={false}/>)
+    reduxRender(<App drawerLatticeDetailsFetching={false} />)
     const linkElement = screen.getByTestId('latticedispatchoverview')
     expect(linkElement).toBeInTheDocument()
   })
 
   test('Dispatch drawer title rendered', () => {
-    reduxRender(<App title={'4be2f8f9-528d-4006-9e13-0ddf84172c98'}/>)
+    reduxRender(<App title={'4be2f8f9-528d-4006-9e13-0ddf84172c98'} />)
     const linkElement = screen.getByTestId('latticedispatchoverview')
     expect(linkElement).toBeInTheDocument()
   })
 
   test('Dispatch Id rendered', () => {
-    reduxRender(<App dispatchId={'4be2f8f9-528d-4006-9e13-0ddf84172c98'}/>)
+    reduxRender(<App dispatchId={'4be2f8f9-528d-4006-9e13-0ddf84172c98'} />)
     const linkElement = screen.getByTestId('latticedispatchoverview')
     expect(linkElement).toBeInTheDocument()
   })
 
   test('copy button rendered', () => {
-    reduxRender(<App content={'4be2f8f9-528d-4006-9e13-0ddf84172c98'}/>)
+    reduxRender(<App content={'4be2f8f9-528d-4006-9e13-0ddf84172c98'} />)
     const linkElement = screen.getByTestId('copydispatchId')
     expect(linkElement).toBeInTheDocument()
   })
 
   test('copy button title rendered', () => {
-    reduxRender(<App title="Copy dispatch Id"/>)
+    reduxRender(<App title="Copy dispatch Id" />)
     const linkElement = screen.getByTestId('copydispatchId')
     expect(linkElement).toBeInTheDocument()
   })
 
   test('error card rendered', () => {
-    reduxRender(<App drawerLatticeDetails={drawerLatticeDetails}/>)
+    reduxRender(<App drawerLatticeDetails={drawerLatticeDetails} />)
     const linkElement = screen.getByTestId('latticedispatchoverview')
     expect(linkElement).toBeInTheDocument()
   })
 
   test('error checking', () => {
-    reduxRender(<App drawerLatticeDetails={drawerLatticeDetails} error="error"/>)
+    reduxRender(
+      <App drawerLatticeDetails={drawerLatticeDetails} error="error" />
+    )
     const linkElement = screen.getByTestId('latticedispatchoverview')
     expect(linkElement).toBeInTheDocument()
   })
@@ -113,20 +115,20 @@
   })
 
   test('tab view value rendering', () => {
-    reduxRender(<App value="overview"/>)
+    reduxRender(<App value="overview" />)
     const linkElement = screen.getByTestId('latticedispatchoverview')
     expect(linkElement).toBeInTheDocument()
   })
 
   test('latticedispatchoverview isFetching rendered', () => {
-    reduxRender(<App isFetching={false}/>)
+    reduxRender(<App isFetching={false} />)
     const linkElement = screen.getByTestId('latticedispatchoverview')
     expect(linkElement).toBeInTheDocument()
   })
 
   test('latticedispatchoverview latDetails rendered', () => {
-    reduxRender(<App latDetails={drawerLatticeDetails}/>)
+    reduxRender(<App latDetails={drawerLatticeDetails} />)
     const linkElement = screen.getByTestId('latticedispatchoverview')
     expect(linkElement).toBeInTheDocument()
   })
- })
+})
