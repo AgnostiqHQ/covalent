3e51b20e77a7a2ba2243703808749f657a73a63d
Prasanna Venkatesh <54540812+Prasy12@users.noreply.github.com>
2022-09-22 12:12:22 +0530

    UI logs feature (#1264)
    * #1185 - Implemented logs API
    
    * #1185 - Fix for logs sorting
    
    * #1185 - list sorting null values fix
    
    * Added logs page
    
    * #1185 - Logs download fix
    
    * Logs page UI change
    
    * Adding new electron statuses
    
    * Electron status changes
    
    * #1185 - logs count fix
    
    * Changes for model helper
    
    * Changes on request and response model
    
    * Added homepage resolution for 1500px
    
    * Logs table UI changes
    
    * Download functionality changes for logs
    
    * #1185 - offset and count conflict issue fix
    
    * Introduce pagination in logs
    
    * Added zoom in and out icons in lattice controls
    
    * #1185 - config changes for log API
    
    * Changed logs table height
    
    * #1185 - Added critical log type
    
    * Add navbar to dispatch not found view.
    
    * Nav bar changes.
    
    * Font size changed and status text color added
    
    * Changes to download on the UI.
    
    * Disable download button when download is in progress.
    
    * Changed logs table height and preview width
    
    * Change cancelled status label color.
    
    * Changes for logs
    
    * #1185 - search logs bug fix
    
    * Graph UI changes
    
    * Fix parameter node position
    
    * Altered the unit tests
    
    * Update changelog for logs
    
    * #1185 - date format bug fix
    
    Co-authored-by: Manjunath PV <manjunath.poilath@psiog.com>
    Co-authored-by: kamalesh.suresh <kamalesh.suresh@psiog.com>
    Co-authored-by: ArunPsiog <arun.mukesh@psiog.com>
    Co-authored-by: RaviPsiog <raviteja.gurram@psiog.com>

--- covalent_ui/api/v1/data_layer/lattice_dal.py
+++ covalent_ui/api/v1/data_layer/lattice_dal.py
@@ -26,7 +26,6 @@ from uuid import UUID
 from sqlalchemy.orm import Session
 from sqlalchemy.sql import func
 
-from covalent_ui.api.v1.database.schema.electron import Electron
 from covalent_ui.api.v1.database.schema.lattices import Lattice
 from covalent_ui.api.v1.models.lattices_model import LatticeDetailResponse
 
@@ -75,7 +74,6 @@ class Lattices:
                     "updated_at"
                 ),
             )
-            .join(Electron, Electron.parent_lattice_id == Lattice.id)
             .filter(Lattice.dispatch_id == str(dispatch_id), Lattice.is_active.is_not(False))
             .first()
         )
@@ -112,7 +110,6 @@ class Lattices:
                 Lattice.electron_num.label("total_electrons"),
                 Lattice.completed_electron_num.label("total_electrons_completed"),
             )
-            .join(Electron, Electron.parent_lattice_id == Lattice.id)
             .filter(Lattice.dispatch_id == str(dispatch_id), Lattice.is_active.is_not(False))
             .first()
         )
