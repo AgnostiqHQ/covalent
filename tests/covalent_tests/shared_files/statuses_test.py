e22e4c7631409e83fc60de3e4d90de8b1a42c43b
Sankalp Sanand <sankalp@agnostiq.ai>
2022-09-29 09:39:30 -0400

    Added tests for status and status_listener

--- tests/covalent_tests/shared_files/statuses_test.py
+++ tests/covalent_tests/shared_files/statuses_test.py
@@ -17,3 +17,82 @@
 # FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
 #
 # Relief from the License may be granted by purchasing a commercial license.
+
+import asyncio
+from unittest import mock
+
+import pytest
+
+from covalent._shared_files.statuses import Status, status_listener
+
+
+@pytest.fixture
+def status():
+    return Status()
+
+
+def test_default_status(status: Status):
+    """Test to check functions of Status are working as expected"""
+
+    assert status.status_name == str(status)
+    identifier = status.get_identifier()
+    ids = identifier.split(":")
+
+    assert status.executor_name in ids
+    assert status.category_name in ids
+    assert status.status_name in ids
+
+
+@pytest.mark.asyncio
+async def test_status_listener_case_1(mocker: mock):
+
+    test_status = "TEST_STATUS"
+
+    sv_mock = mocker.MagicMock()
+    sv_mock.retrieve_async = mocker.AsyncMock(return_value=None)
+
+    result_mock = mocker.MagicMock()
+    result_mock._get_node_status.return_value = test_status
+
+    # Case 1: current_status != node_status
+    result_mock._update_node.side_effect = asyncio.CancelledError
+
+    await status_listener(result_object=result_mock, node_id=-2, status_store=sv_mock)
+    sv_mock.retrieve_async.assert_called_once()
+    result_mock._get_node_status.assert_called_once()
+    result_mock._update_node.assert_called_once()
+
+
+@pytest.mark.asyncio
+async def test_status_listener_case_2(mocker: mock):
+
+    sv_mock = mocker.MagicMock()
+    sv_mock.retrieve_async = mocker.AsyncMock(return_value=None)
+
+    result_mock = mocker.MagicMock()
+    result_mock._get_node_status.return_value = None
+    result_mock._get_node_status.side_effect = asyncio.CancelledError
+
+    # Case 2: current_status == node_status
+    await status_listener(result_object=result_mock, node_id=-2, status_store=sv_mock)
+    sv_mock.retrieve_async.assert_called_once()
+    result_mock._get_node_status.assert_called_once()
+    assert result_mock._update_node.call_count == 0
+
+
+@pytest.mark.asyncio
+async def test_status_listener_case_3(mocker: mock):
+
+    test_status = "TEST_STATUS"
+
+    sv_mock = mocker.MagicMock()
+    sv_mock.retrieve_async = mocker.AsyncMock(return_value=test_status)
+
+    result_mock = mocker.MagicMock()
+    result_mock._get_node_status.return_value = test_status
+
+    # Case 3: type(current_status) != type(None, RunningCategory, PendingCategory)
+    await status_listener(result_object=result_mock, node_id=-2, status_store=sv_mock)
+    sv_mock.retrieve_async.assert_called_once()
+    assert result_mock._get_node_status.call_count == 0
+    result_mock._update_node.assert_called_once()
