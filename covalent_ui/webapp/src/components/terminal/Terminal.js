0f2b02883eb745d784da6ea0999dfe0123e62280
Prasy12 <prasanna.venkatesh@psiog.com>
2022-10-05 15:28:10 +0530

    Terminal UI changes

--- covalent_ui/webapp/src/components/terminal/Terminal.js
+++ covalent_ui/webapp/src/components/terminal/Terminal.js
@@ -4,13 +4,11 @@ import React, { useRef, useEffect, useState } from 'react'
 import { XTerm } from 'xterm-for-react'
 import { FitAddon } from 'xterm-addon-fit';
 import io from 'socket.io-client'
-import { WebLinksAddon } from 'xterm-addon-web-links';
 import { Container, Grid, Chip } from '@mui/material';
 import Typography from '@mui/material/Typography';
-
+import './index.css';
 const Terminal = () => {
     const xtermRef = useRef()
-    const webLinksAddon = new WebLinksAddon()
     const fitAddon = new FitAddon();
     const socket = io(process.env.REACT_APP_SOCKET_URL, {
         // required for CORS
@@ -20,10 +18,12 @@ const Terminal = () => {
         if (socket) {
             socket.on('connect', () => {
                 console.debug(`socket ${socket.id} terminal connected: ${socket.connected}`)
-                socket.emit('start_terminal');
                 fitAddon.fit();
                 const dims = { cols: xtermRef?.current?.terminal.cols, rows: xtermRef?.current?.terminal.rows };
-                if (dims.rows) socket.emit("resize", dims);
+                if (dims.rows) {
+                    socket.emit('start_terminal', dims);
+                    socket.emit("resize", dims);
+                }
             })
         }
     })
@@ -58,14 +58,15 @@ const Terminal = () => {
                         macOptionIsMeta: true,
                         scrollback: true,
                         theme: {
-                            background: '#08081A'
-                        },
+                            background: '#08081A',
+                        }
                     }}
+                    className='term'
                     ref={xtermRef}
                     onData={(data) => {
                         socket.emit("pty-input", { input: data })
                     }}
-                    addons={[fitAddon, webLinksAddon]} />
+                    addons={[fitAddon]} />
             )}
         </Container>
 
