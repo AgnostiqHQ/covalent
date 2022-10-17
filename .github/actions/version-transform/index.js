3db2ef4e027101a5391bde3b330c4a5240b08324
Alejandro Esquivel <ae@alejandro.ltd>
2022-10-17 13:16:55 -0700

    Added re-usable action for version transformation

---
+++ .github/actions/version-transform/index.js
@@ -0,0 +1,11 @@
+const core = require("@actions/core");
+const fs = require('fs')
+const version = fs
+    .readFileSync(core.getInput("version-path"), "utf8")
+    .trim();
+
+version = version.replace(/(-)([0-9]+)/, ".post$2")
+version = version.replace(/(.post[0-9]+)(-rc.)/, "$1.dev")
+version = version.replace('-rc.', 'rc')
+
+core.setOutput("version", version);
