b9c00e1bb1a0b0551df8e67fce539641d52cc93a
Casey Jao <casey@agnostiq.ai>
2022-10-27 10:00:05 -0400

    Don't show stdout in Electron ErrorCard
    The ErrorCard shouldn't appear for normally running electrons.

--- covalent_ui/api/v1/routes/end_points/electron_routes.py
+++ covalent_ui/api/v1/routes/end_points/electron_routes.py
@@ -158,8 +158,7 @@ def get_electron_file(dispatch_id: uuid.UUID, electron_id: int, name: ElectronFi
                 # is only used for fatal dispatcher-executor interaction errors
                 error_response = handler.read_from_text(result["error_filename"])
                 stderr_response = handler.read_from_text(result["stderr_filename"])
-                stdout_response = handler.read_from_text(result["stdout_filename"])
-                response = stdout_response + stderr_response + error_response
+                response = stderr_response + error_response
                 return ElectronFileResponse(data=response)
             else:
                 return ElectronFileResponse(data=None)
