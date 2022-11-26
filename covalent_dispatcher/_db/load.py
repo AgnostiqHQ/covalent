e6101d747aa43e744cd5ed3777dfb2e83f1e1423
Casey Jao <casey@agnostiq.ai>
2022-11-26 09:07:42 -0500

    Add methods to rehydrate a transport graph from db

---
+++ covalent_dispatcher/_db/load.py
@@ -0,0 +1,113 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+"""This module rehydrates a transport graph from the db"""
+
+from typing import Dict, List, Tuple
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+
+from covalent._shared_files import logger
+from covalent._workflow.transport import _TransportGraph
+
+from .datastore import workflow_db
+from .models import Electron, ElectronDependency, Lattice
+
+app_log = logger.app_log
+log_stack_info = logger.log_stack_info
+
+
+class MissingElectronRecordError(Exception):
+    pass
+
+
+def _node_records(session: Session, dispatch_id: str) -> List[Electron]:
+    stmt = (
+        select(Electron)
+        .join(Lattice, Lattice.id == Electron.parent_lattice_id)
+        .where(Lattice.dispatch_id == dispatch_id)
+    )
+    records = session.execute(stmt).all()
+    return list(map(lambda r: r.Electron, records))
+
+
+# Join electron dependency with filtered electrons on destination electron
+def _edge_records(session: Session, dispatch_id: str) -> List[ElectronDependency]:
+    stmt = (
+        select(ElectronDependency)
+        .join(Electron, Electron.id == ElectronDependency.electron_id)
+        .join(Lattice, Lattice.id == Electron.parent_lattice_id)
+        .where(Lattice.dispatch_id == dispatch_id)
+    )
+    records = session.execute(stmt).all()
+    return list(map(lambda r: r.ElectronDependency, records))
+
+
+def _to_abstract_node(e_record: Electron) -> Tuple[int, Dict]:
+    node_id = e_record.transport_graph_node_id
+    attrs = {
+        "uid": e_record.id,
+        "type": e_record.type,
+        "job_id": e_record.job_id,
+    }
+    return node_id, attrs
+
+
+def _to_abstract_edge(
+    e_record: ElectronDependency, uid_node_id_map: Dict
+) -> Tuple[int, int, Dict]:
+    x = uid_node_id_map[e_record.parent_electron_id]
+    y = uid_node_id_map[e_record.electron_id]
+    attrs = {
+        "edge_name": e_record.edge_name,
+        "parameter_type": e_record.parameter_type,
+        "arg_index": e_record.arg_index,
+    }
+
+    return x, y, attrs
+
+
+def _abstract_nodes_and_edges(session: Session, dispatch_id: str):
+
+    db_nodes = _node_records(session, dispatch_id)
+    db_edges = _edge_records(session, dispatch_id)
+    uid_nodeid_map = {e.id: e.transport_graph_node_id for e in db_nodes}
+    nodes = list(map(_to_abstract_node, db_nodes))
+    edges = list(map(lambda x: _to_abstract_edge(x, uid_nodeid_map), db_edges))
+
+    return nodes, edges
+
+
+def _make_transport_graph(nodes: List, edges: List) -> _TransportGraph:
+    tg = _TransportGraph()
+    for node in nodes:
+        node_id, attrs = node
+        tg.add_node_by_id(node_id, **attrs)
+    for edge in edges:
+        x, y, attrs = edge
+        tg.add_edge(x, y, **attrs)
+    return tg
+
+
+def abstract_tg(dispatch_id: str) -> _TransportGraph:
+    with workflow_db.Session() as session:
+        nodes, edges = _abstract_nodes_and_edges(session, dispatch_id)
+    return _make_transport_graph(nodes, edges)
---
+++ tests/covalent_dispatcher_tests/_db/load_test.py
@@ -0,0 +1,262 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+"""Unit tests for the module used to load transport graphs from the DB."""
+
+from datetime import datetime as dt
+from datetime import timezone
+
+import pytest
+
+import covalent as ct
+from covalent._shared_files.defaults import parameter_prefix, prefix_separator
+from covalent_dispatcher._db.datastore import DataStore
+from covalent_dispatcher._db.load import abstract_tg
+from covalent_dispatcher._db.write_result_to_db import (
+    get_electron_type,
+    insert_electron_dependency_data,
+    insert_electrons_data,
+    insert_lattices_data,
+)
+
+STORAGE_TYPE = "local"
+FUNCTION_FILENAME = "dispatch_source.pkl"
+FUNCTION_STRING_FILENAME = "dispatch_source.py"
+DOCSTRING_FILENAME = "dispatch_source_docstring.txt"
+EXECUTOR_DATA_FILENAME = "executor_data.pkl"
+WORKFLOW_EXECUTOR_DATA_FILENAME = "workflow_executor_data.pkl"
+ERROR_FILENAME = "error.txt"
+INPUTS_FILENAME = "inputs.pkl"
+NAMED_ARGS_FILENAME = "named_args.pkl"
+NAMED_KWARGS_FILENAME = "named_kwargs.pkl"
+RESULTS_FILENAME = "results.pkl"
+VALUE_FILENAME = "value.pkl"
+STDOUT_FILENAME = "stdout.log"
+STDERR_FILENAME = "stderr.log"
+ERROR_FILENAME = "error.log"
+TRANSPORT_GRAPH_FILENAME = "transport_graph.pkl"
+DEPS_FILENAME = "deps.pkl"
+CALL_BEFORE_FILENAME = "call_before.pkl"
+CALL_AFTER_FILENAME = "call_after.pkl"
+COVA_IMPORTS_FILENAME = "cova_imports.pkl"
+LATTICE_IMPORTS_FILENAME = "lattice_imports.pkl"
+RESULTS_DIR = "/tmp/results"
+
+
+@pytest.fixture
+def test_db():
+    """Instantiate and return an in-memory database."""
+
+    return DataStore(
+        db_URL="sqlite+pysqlite:///:memory:",
+        initialize_db=True,
+    )
+
+
+@pytest.fixture
+def workflow_lattice():
+    @ct.electron
+    def task_1(x, y):
+        return x * y
+
+    @ct.electron
+    def sublattice_task(z):
+        return z
+
+    @ct.electron
+    @ct.lattice
+    def task_2(z):
+        return sublattice_task(z)
+
+    @ct.lattice
+    def workflow_1(a, b):
+        res_1 = task_1(a, b)
+        return task_2(res_1, b)
+
+    workflow_1.build_graph(a=1, b=2)
+
+    return workflow_1
+
+
+def get_lattice_kwargs(
+    dispatch_id="dispatch_1",
+    electron_id=None,
+    name="workflow_1",
+    docstring_filename=DOCSTRING_FILENAME,
+    status="RUNNING",
+    electron_num=6,
+    completed_electron_num=0,
+    storage_type=STORAGE_TYPE,
+    storage_path="results/dispatch_1/",
+    function_filename=FUNCTION_FILENAME,
+    function_string_filename=FUNCTION_STRING_FILENAME,
+    executor="dask",
+    executor_data_filename=EXECUTOR_DATA_FILENAME,
+    workflow_executor="dask",
+    workflow_executor_data_filename=WORKFLOW_EXECUTOR_DATA_FILENAME,
+    error_filename=ERROR_FILENAME,
+    inputs_filename=INPUTS_FILENAME,
+    named_args_filename=NAMED_ARGS_FILENAME,
+    named_kwargs_filename=NAMED_KWARGS_FILENAME,
+    results_filename=RESULTS_FILENAME,
+    transport_graph_filename=TRANSPORT_GRAPH_FILENAME,
+    deps_filename=DEPS_FILENAME,
+    call_before_filename=CALL_BEFORE_FILENAME,
+    call_after_filename=CALL_AFTER_FILENAME,
+    cova_imports_filename=COVA_IMPORTS_FILENAME,
+    lattice_imports_filename=LATTICE_IMPORTS_FILENAME,
+    results_dir=RESULTS_DIR,
+    root_dispatch_id="dispatch_1",
+    created_at=None,
+    updated_at=None,
+    started_at=None,
+    completed_at=None,
+):
+    """Create lattice kwargs."""
+
+    return {
+        "dispatch_id": dispatch_id,
+        "electron_id": electron_id,
+        "name": name,
+        "docstring_filename": docstring_filename,
+        "status": status,
+        "electron_num": electron_num,
+        "completed_electron_num": completed_electron_num,
+        "storage_type": storage_type,
+        "storage_path": storage_path,
+        "function_filename": function_filename,
+        "function_string_filename": function_string_filename,
+        "executor": executor,
+        "executor_data_filename": executor_data_filename,
+        "workflow_executor": workflow_executor,
+        "workflow_executor_data_filename": workflow_executor_data_filename,
+        "error_filename": error_filename,
+        "inputs_filename": inputs_filename,
+        "named_args_filename": named_args_filename,
+        "named_kwargs_filename": named_kwargs_filename,
+        "results_filename": results_filename,
+        "transport_graph_filename": transport_graph_filename,
+        "deps_filename": deps_filename,
+        "call_before_filename": call_before_filename,
+        "call_after_filename": call_after_filename,
+        "cova_imports_filename": cova_imports_filename,
+        "lattice_imports_filename": lattice_imports_filename,
+        "results_dir": results_dir,
+        "root_dispatch_id": root_dispatch_id,
+        "created_at": created_at,
+        "updated_at": updated_at,
+        "started_at": started_at,
+        "completed_at": completed_at,
+    }
+
+
+def get_electron_kwargs(
+    parent_dispatch_id="dispatch_1",
+    transport_graph_node_id=0,
+    type=parameter_prefix.strip(prefix_separator),
+    name=f"{parameter_prefix}0",
+    status="NEW_OBJ",
+    storage_type=STORAGE_TYPE,
+    storage_path="results/dispatch_1/node_0/",
+    function_filename=FUNCTION_STRING_FILENAME,
+    function_string_filename=FUNCTION_STRING_FILENAME,
+    executor="dask",
+    executor_data_filename=EXECUTOR_DATA_FILENAME,
+    results_filename=RESULTS_FILENAME,
+    value_filename=VALUE_FILENAME,
+    stdout_filename=STDOUT_FILENAME,
+    stderr_filename=STDERR_FILENAME,
+    error_filename=ERROR_FILENAME,
+    deps_filename=DEPS_FILENAME,
+    cancel_requested=False,
+    call_before_filename=CALL_BEFORE_FILENAME,
+    call_after_filename=CALL_AFTER_FILENAME,
+    created_at=None,
+    updated_at=None,
+    started_at=None,
+    completed_at=None,
+):
+    """Create electron kwargs."""
+
+    return {
+        "parent_dispatch_id": parent_dispatch_id,
+        "transport_graph_node_id": transport_graph_node_id,
+        "type": type,
+        "name": name,
+        "status": status,
+        "storage_type": storage_type,
+        "storage_path": storage_path,
+        "function_filename": function_filename,
+        "function_string_filename": function_string_filename,
+        "executor": executor,
+        "executor_data_filename": executor_data_filename,
+        "results_filename": results_filename,
+        "value_filename": value_filename,
+        "stdout_filename": stdout_filename,
+        "stderr_filename": stderr_filename,
+        "error_filename": error_filename,
+        "deps_filename": deps_filename,
+        "call_before_filename": call_before_filename,
+        "call_after_filename": call_after_filename,
+        "cancel_requested": cancel_requested,
+        "created_at": created_at,
+        "updated_at": updated_at,
+        "started_at": started_at,
+        "completed_at": completed_at,
+    }
+
+
+def test_abstract_tg(test_db, workflow_lattice, mocker):
+
+    mocker.patch("covalent_dispatcher._db.write_result_to_db.workflow_db", test_db)
+    mocker.patch("covalent_dispatcher._db.load.workflow_db", test_db)
+    lattice_kwargs = get_lattice_kwargs()
+    insert_lattices_data(**lattice_kwargs)
+    cur_time = dt.now(timezone.utc)
+    for (name, node_id) in [
+        ("task_1", 0),
+        (":parameter:1", 1),
+        (":parameter:2", 2),
+        (":sublattice:task_2", 3),
+        (":parameter:2", 4),
+    ]:
+        electron_kwargs = get_electron_kwargs(
+            name=name,
+            type=get_electron_type(name),
+            transport_graph_node_id=node_id,
+            created_at=cur_time,
+            updated_at=cur_time,
+        )
+        insert_electrons_data(**electron_kwargs)
+
+    insert_electron_dependency_data("dispatch_1", workflow_lattice)
+
+    tg = abstract_tg("dispatch_1")
+
+    assert list(tg._graph.nodes) == [0, 1, 2, 3, 4]
+    assert tg.get_node_value(0, "type") == "function"
+    assert tg.get_node_value(1, "type") == "parameter"
+    assert tg.get_node_value(2, "type") == "parameter"
+    assert tg.get_node_value(3, "type") == "sublattice"
+    assert tg.get_node_value(4, "type") == "parameter"
+    assert (1, 0, 0) in tg._graph.edges
+    assert (2, 0, 0) in tg._graph.edges
+    assert (0, 3, 0) in tg._graph.edges
+    assert (4, 3, 0) in tg._graph.edges
