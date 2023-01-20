918534def46c6d0e04b57d33398991d97df38d9d
Casey Jao <casey@agnostiq.ai>
2023-01-20 16:37:49 -0500

    Cov-Ex: Don't use address_client_map for Managed Dask mode

--- tests/covalent_tests/executor/executor_plugins/managed_dask_test.py
+++ tests/covalent_tests/executor/executor_plugins/managed_dask_test.py
@@ -108,3 +108,89 @@ async def test_managed_dask_send_poll_receive():
     os.unlink(output_uri)
     os.unlink(stdout_uri)
     os.unlink(stderr_uri)
+
+
+@pytest.mark.skipif(
+    not os.environ.get("DASK_SCHEDULER_ADDR"), reason="DASK_SCHEDULER_ADDR not set"
+)
+@pytest.mark.asyncio
+async def test_managed_dask_handles_runtime_exceptions():
+    """Test runtime error handling"""
+
+    from dask.distributed import Future
+
+    from covalent.executor.executor_plugins.managed_dask import (
+        ManagedDaskExecutor,
+        _clients,
+        _futures,
+    )
+
+    scheduler_address = os.environ.get("DASK_SCHEDULER_ADDR", None)
+    if not scheduler_address:
+        raise RuntimeError("DASK_SCHEDULER_ADDR not set")
+
+    executor = ManagedDaskExecutor(scheduler_address, cache_dir="/tmp")
+
+    def task(x, y):
+        import sys
+
+        print("HELLO!!")
+        print("Error", file=sys.stderr)
+        raise RuntimeError("RUNTIMEERROR")
+
+    serialized_fn = TransportableObject(task)
+    serialized_x = TransportableObject(3)
+    serialized_y = TransportableObject(1)
+
+    with tempfile.NamedTemporaryFile("wb", delete=False, suffix=".pkl") as temp:
+        function_uri = temp.name
+        cloudpickle.dump(serialized_fn, temp)
+
+    with tempfile.NamedTemporaryFile("wb", delete=False, suffix=".pkl") as temp:
+        arg_uri = temp.name
+        cloudpickle.dump(serialized_x, temp)
+
+    with tempfile.NamedTemporaryFile("wb", delete=False, suffix=".pkl") as temp:
+        kwarg_uri = temp.name
+        cloudpickle.dump(serialized_y, temp)
+
+    dispatch_id = "failed_dispatch"
+    node_id = 2
+    task_metadata = {"dispatch_id": dispatch_id, "node_id": node_id}
+
+    key = await executor.send(
+        f"file://{function_uri}",
+        "",
+        "",
+        "",
+        [f"file://{arg_uri}"],
+        {"y": kwarg_uri},
+        task_metadata,
+    )
+
+    print("DEBUG: clients", _clients)
+    print("DEBUG: futures", _futures)
+
+    client = _clients[key]
+
+    fut = _futures[key]
+
+    fut_2 = Future(key=key, client=client)
+    await executor.poll(task_metadata, key)
+
+    print(fut)
+    print(fut_2)
+    assert fut_2.done()
+
+    output_uri, stdout_uri, stderr_uri, exception_raised = await executor.receive(
+        task_metadata, key
+    )
+
+    assert not output_uri
+
+    with open(stderr_uri, "r") as f:
+        assert "RUNTIMEERROR" in f.read()
+
+    os.unlink(function_uri)
+    os.unlink(stdout_uri)
+    os.unlink(stderr_uri)
