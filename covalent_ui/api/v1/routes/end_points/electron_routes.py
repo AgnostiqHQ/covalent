81905dedc79e09cad85475beca200293ca9fbe71
Casey Jao <casey@agnostiq.ai>
2023-01-20 09:43:59 -0500

    Cov-Ex: Remove _get_task_inputs hack for UI

--- covalent_ui/api/v1/routes/end_points/electron_routes.py
+++ covalent_ui/api/v1/routes/end_points/electron_routes.py
@@ -20,13 +20,15 @@
 
 """Electrons Route"""
 
-import asyncio
 import uuid
 
 from fastapi import APIRouter, HTTPException
 from sqlalchemy.orm import Session
 
+from covalent._results_manager import Result
 from covalent._results_manager.results_manager import get_result
+from covalent._shared_files.defaults import electron_dict_prefix, electron_list_prefix
+from covalent._workflow.transport import TransportableObject
 from covalent_ui.api.v1.data_layer.electron_dal import Electrons
 from covalent_ui.api.v1.database.config.db import engine
 from covalent_ui.api.v1.models.electrons_model import (
@@ -79,6 +81,62 @@ def get_electron_details(dispatch_id: uuid.UUID, electron_id: int):
         )
 
 
+# Copied from execution.py
+def get_task_inputs(node_id: int, node_name: str, result_object: Result) -> dict:
+    """
+    Return the required inputs for a task execution.
+    This makes sure that any node with child nodes isn't executed twice and fetches the
+    result of parent node to use as input for the child node.
+    Args:
+        node_id: Node id of this task in the transport graph.
+        node_name: Name of the node.
+        result_object: Result object to be used to update and store execution related
+                       info including the results.
+    Returns:
+        inputs: Input dictionary to be passed to the task containing args, kwargs,
+                and any parent node execution results if present.
+    """
+
+    if node_name.startswith(electron_list_prefix):
+        values = [
+            result_object.lattice.transport_graph.get_node_value(parent, "output")
+            for parent in result_object.lattice.transport_graph.get_dependencies(node_id)
+        ]
+        task_input = {"args": [], "kwargs": {"x": TransportableObject.make_transportable(values)}}
+    elif node_name.startswith(electron_dict_prefix):
+        values = {}
+        for parent in result_object.lattice.transport_graph.get_dependencies(node_id):
+
+            edge_data = result_object.lattice.transport_graph.get_edge_data(parent, node_id)
+
+            value = result_object.lattice.transport_graph.get_node_value(parent, "output")
+            for e_key, d in edge_data.items():
+                key = d["edge_name"]
+                values[key] = value
+
+        task_input = {"args": [], "kwargs": {"x": TransportableObject.make_transportable(values)}}
+    else:
+        task_input = {"args": [], "kwargs": {}}
+
+        for parent in result_object.lattice.transport_graph.get_dependencies(node_id):
+
+            edge_data = result_object.lattice.transport_graph.get_edge_data(parent, node_id)
+            value = result_object.lattice.transport_graph.get_node_value(parent, "output")
+
+            for e_key, d in edge_data.items():
+                if not d.get("wait_for"):
+                    if d["param_type"] == "arg":
+                        task_input["args"].append((value, d["arg_index"]))
+                    elif d["param_type"] == "kwarg":
+                        key = d["edge_name"]
+                        task_input["kwargs"][key] = value
+
+        sorted_args = sorted(task_input["args"], key=lambda x: x[1])
+        task_input["args"] = [x[0] for x in sorted_args]
+
+    return task_input
+
+
 def get_electron_inputs(dispatch_id: uuid.UUID, electron_id: int) -> str:
     """
     Get Electron Inputs
@@ -88,20 +146,15 @@ def get_electron_inputs(dispatch_id: uuid.UUID, electron_id: int) -> str:
     Returns:
         Returns the inputs data from Result object
     """
-    from covalent_dispatcher._core.execution import _event_loop as event_loop
-    from covalent_dispatcher._core.execution import _get_task_inputs as get_task_inputs
 
     result_object = get_result(dispatch_id=str(dispatch_id), wait=False)
 
     with Session(engine) as session:
         electron = Electrons(session)
         result = electron.get_electrons_id(dispatch_id, electron_id)
-        inputs = asyncio.run_coroutine_threadsafe(
-            get_task_inputs(
-                node_id=electron_id, node_name=result.name, result_object=result_object
-            ),
-            event_loop,
-        ).result()
+        inputs = get_task_inputs(
+            node_id=electron_id, node_name=result.name, result_object=result_object
+        )
         return validate_data(inputs)
 
 
