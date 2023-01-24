5f85823054676859acd1d6bb6c44b56fb7fea2c3
Faiyaz Hasan <faiyaz@agnostiq.ai>
2023-01-24 16:58:41 -0500

    add generate electron updates test

--- tests/covalent_tests/dispatcher_plugins/redispatch_utils_test.py
+++ tests/covalent_tests/dispatcher_plugins/redispatch_utils_test.py
@@ -22,10 +22,13 @@
 
 import pytest
 
+import covalent as ct
 from covalent._dispatcher_plugins.redispatch_utils import (
     _filter_null_metadata,
+    _generate_electron_updates,
     _get_transportable_electron,
 )
+from covalent._shared_files.context_managers import active_lattice_manager
 from covalent._workflow.electron import Electron
 from covalent._workflow.transport import TransportableObject
 
@@ -46,18 +49,20 @@ def test_filter_null_metadata(meta_dict, expected):
 
 def test_get_transportable_electron(mocker):
     """Test the get transportable electron function."""
-    get_serialized_function_str_mock = mocker.patch(
+    mocker.patch(
         "covalent._dispatcher_plugins.redispatch_utils.get_serialized_function_str",
         return_value="mock-function-string",
     )
-    filter_null_metadata_mock = mocker.patch(
+    mocker.patch(
         "covalent._dispatcher_plugins.redispatch_utils._filter_null_metadata",
         return_value="mock-metadata",
     )
 
+    @ct.electron
     def test_func(a):
         return a
 
+    # Construct bound electron, i.e. electron with non-null function and node_id
     electron = Electron(function=test_func, node_id=1, metadata={"a": 1, "b": 2})
     transportable_electron = _get_transportable_electron(electron)
 
@@ -67,9 +72,41 @@ def test_get_transportable_electron(mocker):
     assert TransportableObject(test_func).to_dict() == transportable_electron["function"]
 
 
-def test_generate_electron_updates():
+def test_generate_electron_updates(mocker):
     """Test the generate electron updates function."""
-    pass
+    mocker.patch(
+        "covalent._dispatcher_plugins.redispatch_utils._get_transportable_electron",
+        side_effect=["mock-transportable-electron-1", "mock-transportable-electron-2"],
+    )
+
+    @ct.electron
+    def identity(a):
+        return a
+
+    @ct.electron
+    def add(a, b):
+        return a + b
+
+    @ct.lattice
+    def workflow():
+        pass
+
+    # Construct bound electrons, i.e. electron with non-null function and node_id
+    identity_electron = Electron(function=identity, node_id=1, metadata={"executor": "local"})
+    add_electron = Electron(function=add, node_id=2, metadata={"executor": "local"})
+
+    with active_lattice_manager.claim(workflow):
+        electron_updates = _generate_electron_updates(
+            "mock-dispatch-id",
+            {
+                "mock_task_id_1": identity,
+                "mock_task_id_2": add,
+            },
+        )
+    assert electron_updates == {
+        "mock_task_id_1": "mock-transportable-electron-1",
+        "mock_task_id_2": "mock-transportable-electron-2",
+    }
 
 
 def test_get_request_body():
