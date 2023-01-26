9a45fb2ccb3b055ab9f13572d7b024efc7610353
kessler-frost <ssanand@hawk.iit.edu>
2023-01-26 15:26:52 -0500

    Added experimental qelectron support

---
+++ covalent/experimental/import_experimental_pkgs.py
@@ -0,0 +1,35 @@
+# Copyright 2023 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+
+import contextlib
+
+pkg_names = []
+
+with contextlib.suppress(ImportError):
+    # qelectron
+    import covalent_qelectron
+
+    pkg_names.append(covalent_qelectron.__name__)
+
+    # Add plugin imports at the end only to not
+    # mess with previously imported packages
+
+__all__ = pkg_names
