f5786c15b21da6fc1e740f3689ecd06ddedf3dd4
Casey Jao <casey@agnostiq.ai>
2023-02-02 10:10:43 -0500

    Cleanup: revert move dask helper fn

--- covalent/executor/executor_plugins/dask.py
+++ covalent/executor/executor_plugins/dask.py
@@ -25,9 +25,14 @@ and waits for execution to finish then returns the result.
 This is a plugin executor module; it is loaded if found and properly structured.
 """
 
+import json
 import os
-from typing import Any, Callable, Dict, List
+import sys
+import traceback
+from contextlib import redirect_stderr, redirect_stdout
+from typing import Any, Callable, Dict, List, Tuple
 
+import cloudpickle as pickle
 from dask.distributed import Client, Future
 
 from covalent._shared_files import TaskRuntimeError, logger
@@ -36,9 +41,12 @@ from covalent._shared_files import TaskRuntimeError, logger
 from covalent._shared_files.config import get_config
 from covalent._shared_files.util_classes import RESULT_STATUS
 from covalent._shared_files.utils import _address_client_mapper
+from covalent._workflow.depsbash import DepsBash
+from covalent._workflow.depscall import DepsCall
+from covalent._workflow.depspip import DepsPip
 from covalent.executor.base import AsyncBaseExecutor
 from covalent.executor.utils.wrappers import io_wrapper as dask_wrapper
-from covalent.executor.utils.wrappers import run_task_from_uris
+from covalent.executor.utils.wrappers import wrapper_fn
 
 # The plugin class name must be given by the executor_plugin_name attribute:
 EXECUTOR_PLUGIN_NAME = "DaskExecutor"
@@ -63,6 +71,129 @@ _clients = {}
 # See
 # https://stackoverflow.com/questions/62164283/why-do-my-dask-futures-get-stuck-in-pending-and-never-finish
 _futures = {}
+
+
+# Copied from runner.py
+def _gather_deps(deps, call_before_objs_json, call_after_objs_json) -> Tuple[List, List]:
+    """Assemble deps for a node into the final call_before and call_after"""
+
+    call_before = []
+    call_after = []
+
+    # Rehydrate deps from JSON
+    if "bash" in deps:
+        dep = DepsBash()
+        dep.from_dict(deps["bash"])
+        call_before.append(dep.apply())
+
+    if "pip" in deps:
+        dep = DepsPip()
+        dep.from_dict(deps["pip"])
+        call_before.append(dep.apply())
+
+    for dep_json in call_before_objs_json:
+        dep = DepsCall()
+        dep.from_dict(dep_json)
+        call_before.append(dep.apply())
+
+    for dep_json in call_after_objs_json:
+        dep = DepsCall()
+        dep.from_dict(dep_json)
+        call_after.append(dep.apply())
+
+    return call_before, call_after
+
+
+# URIs are just file paths
+def run_task_from_uris(
+    function_uri: str,
+    deps_uri: str,
+    call_before_uri: str,
+    call_after_uri: str,
+    args_uris: str,
+    kwargs_uris: str,
+    output_uri: str,
+    stdout_uri: str,
+    stderr_uri: str,
+    results_dir: str,
+    task_metadata: dict,
+):
+
+    prefix = "file://"
+    prefix_len = len(prefix)
+
+    with open(stdout_uri, "w") as stdout, open(stderr_uri, "w") as stderr:
+        with redirect_stdout(stdout), redirect_stderr(stderr):
+            try:
+                if function_uri.startswith(prefix):
+                    function_uri = function_uri[prefix_len:]
+
+                with open(function_uri, "rb") as f:
+                    serialized_fn = pickle.load(f)
+
+                ser_args = []
+                ser_kwargs = {}
+                for uri in args_uris:
+                    if uri.startswith(prefix):
+                        uri = uri[prefix_len:]
+                    with open(uri, "rb") as f:
+                        ser_args.append(pickle.load(f))
+
+                for key, uri in kwargs_uris.items():
+                    if uri.startswith(prefix):
+                        uri = uri[prefix_len:]
+                    with open(uri, "rb") as f:
+                        ser_kwargs[key] = pickle.load(f)
+
+                if deps_uri.startswith(prefix):
+                    deps_uri = deps_uri[prefix_len:]
+                with open(deps_uri, "rb") as f:
+                    deps_json = pickle.load(f)
+
+                if call_before_uri.startswith(prefix):
+                    call_before_uri = call_before_uri[prefix_len:]
+                with open(call_before_uri, "rb") as f:
+                    call_before_json = pickle.load(f)
+
+                if call_after_uri.startswith(prefix):
+                    call_after_uri = call_after_uri[prefix_len:]
+                with open(call_after_uri, "rb") as f:
+                    call_after_json = pickle.load(f)
+
+                call_before, call_after = _gather_deps(
+                    deps_json, call_before_json, call_after_json
+                )
+
+                exception_occurred = False
+
+                ser_output = wrapper_fn(
+                    serialized_fn, call_before, call_after, *ser_args, **ser_kwargs
+                )
+
+                with open(output_uri, "wb") as f:
+                    pickle.dump(ser_output, f)
+
+            except Exception as ex:
+                exception_occurred = True
+                tb = "".join(traceback.TracebackException.from_exception(ex).format())
+                print(tb, file=sys.stderr)
+                output_uri = None
+
+    result_summary = {
+        "output_uri": output_uri,
+        "stdout_uri": stdout_uri,
+        "stderr_uri": stderr_uri,
+        "exception_occurred": exception_occurred,
+    }
+
+    dispatch_id = task_metadata["dispatch_id"]
+    node_id = task_metadata["node_id"]
+    result_path = os.path.join(results_dir, f"result-{dispatch_id}:{node_id}.json")
+
+    with open(result_path, "w") as f:
+        json.dump(result_summary, f)
+
+    return output_uri, stdout_uri, stderr_uri, exception_occurred
 
 
 class DaskExecutor(AsyncBaseExecutor):
@@ -167,6 +298,8 @@ class DaskExecutor(AsyncBaseExecutor):
             output_uri=output_uri,
             stdout_uri=stdout_uri,
             stderr_uri=stderr_uri,
+            results_dir=self.cache_dir,
+            task_metadata=task_metadata,
             key=key,
         )
 
@@ -215,7 +348,16 @@ class DaskExecutor(AsyncBaseExecutor):
 
         # Result should be ready to read immediately
 
-        output_uri, stdout_uri, stderr_uri, exception_raised = await fut.result(1)
+        # output_uri, stdout_uri, stderr_uri, exception_raised = await fut.result(1)
+
+        result_path = os.path.join(self.cache_dir, f"result-{dispatch_id}:{node_id}.json")
+        with open(result_path, "r") as f:
+            result_summary = json.load(f)
+
+        output_uri = result_summary["output_uri"]
+        stdout_uri = result_summary["stdout_uri"]
+        stderr_uri = result_summary["stderr_uri"]
+        exception_raised = result_summary["exception_occurred"]
 
         terminal_status = RESULT_STATUS.FAILED if exception_raised else RESULT_STATUS.COMPLETED
 
