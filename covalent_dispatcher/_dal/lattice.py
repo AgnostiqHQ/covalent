5035e91d5d81a9853b43c43afb970cd84bdcd6ac
Casey Jao <casey@agnostiq.ai>
2023-03-13 00:56:33 -0400

    DAL: rename Asset.name to Asset.key

--- covalent_dispatcher/_dal/lattice.py
+++ covalent_dispatcher/_dal/lattice.py
@@ -52,8 +52,8 @@ class Lattice(DispatchedObject):
         self._storage_path = db_metadata["storage_path"]
         self._storage_type = db_metadata["storage_type"]
 
-        for name in asset_metadata:
-            self._assets[name] = Asset(name, self._storage_path, asset_metadata[name])
+        for key in asset_metadata:
+            self._assets[key] = Asset(key, self._storage_path, asset_metadata[key])
 
         self.transport_graph = get_compute_graph(self._lattice_id, bare)
 
