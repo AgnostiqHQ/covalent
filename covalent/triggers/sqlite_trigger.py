679389b108683f1d0f8cbd90b42fc6b05eda99a2
kessler-frost <ssanand@hawk.iit.edu>
2023-03-15 17:23:42 -0400

    added sqlite trigger

---
+++ covalent/triggers/sqlite_trigger.py
@@ -0,0 +1,97 @@
+# Copyright 2023 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+import sqlite3
+import time
+from threading import Event
+
+from covalent._shared_files import logger
+
+from .base import BaseTrigger
+
+app_log = logger.app_log
+log_stack_info = logger.log_stack_info
+
+
+class SQLiteTrigger(BaseTrigger):
+    """
+    SQLite Trigger which can read for new row additions
+
+    """
+
+    def __init__(
+        self,
+        db_path: str,
+        table_name: str,
+        lattice_dispatch_id: str = None,
+        dispatcher_addr: str = None,
+        triggers_server_addr: str = None,
+    ):
+        super().__init__(lattice_dispatch_id, dispatcher_addr, triggers_server_addr)
+
+        self.stop_flag = None
+        self.last_id = 1
+
+        self.db_path = db_path
+        self.table_name = table_name
+
+    def observe(self) -> None:
+        """
+        Keep performing the trigger action whenever ...
+        until stop condition has been met.
+        """
+
+        app_log.warning("Inside SQLiteTrigger's observe")
+
+        connection = sqlite3.connect(self.db_path)
+        cursor = connection.cursor()
+
+        self.stop_flag = Event()
+        while not self.stop_flag.is_set():
+            app_log.warning("Going to read the last entry in DB")
+
+            # Check if there are any new rows in the database
+            cursor.execute(f"SELECT COUNT(*) FROM {self.table_name} WHERE id > {self.last_id}")
+            count = cursor.fetchone()[0]
+            app_log.warning(f"first row is: {count}")
+
+            if count > 0:
+                # Get the newest row in the database
+                cursor.execute(f"SELECT * FROM {self.table_name} ORDER BY id DESC LIMIT 1")
+                row = cursor.fetchone()
+
+                app_log.warning(f"Row is: {row}")
+                # Update the last_id to the newest row id
+                self.last_id = row[0]
+
+                # Trigger the workflow
+                self.trigger()
+
+            time.sleep(1)
+
+        cursor.close()
+        connection.close()
+
+    def stop(self) -> None:
+        """
+        Stop the running `self.observe()` method by setting the `self.stop_flag` flag.
+        """
+
+        self.stop_flag.set()
