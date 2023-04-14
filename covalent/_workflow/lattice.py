b27aae7e48b9ddf4ef8e53a2e0d0a57453e67767
Faiyaz Hasan <faiyaz@agnostiq.ai>
2023-04-14 11:06:09 -0400

    reverse if condition

--- covalent/_workflow/lattice.py
+++ covalent/_workflow/lattice.py
@@ -246,10 +246,10 @@ class Lattice:
 
         pp = Postprocessor(lattice=self)
 
-        if get_config("sdk.exhaustive_postprocess") != "true":
-            pp.add_reconstruct_postprocess_node(retval, self._bound_electrons.copy())
-        else:
+        if get_config("sdk.exhaustive_postprocess") == "true":
             pp.add_exhaustive_postprocess_node(self._bound_electrons.copy())
+        else:
+            pp.add_reconstruct_postprocess_node(retval, self._bound_electrons.copy())
 
         self._bound_electrons = {}  # Reset bound electrons
 
