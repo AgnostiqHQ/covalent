2c396ca69e3d9016112e66e6e1d1e676e457c773
Faiyaz Hasan <faiyaz@agnostiq.ai>
2023-04-20 08:48:49 -0400

    Moved basic workflow to functional tests

---
+++ tests/functional_tests/qa_basic_workflow_test.py
@@ -0,0 +1,44 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+
+"""QA script to test basic workflow functionality."""
+
+import covalent as ct
+
+
+def test_basic_workflow():
+    @ct.electron
+    def join_words(a, b):
+        return ", ".join([a, b])
+
+    @ct.electron
+    def excitement(a):
+        return f"{a}!"
+
+    @ct.lattice
+    def simple_workflow(a, b):
+        phrase = join_words(a, b)
+        return excitement(phrase)
+
+    dispatch_id = ct.dispatch(simple_workflow)("Hello", "World")
+    res = ct.get_result(dispatch_id, wait=True)
+    assert res.result == "Hello, World!"
+    assert res.status == "COMPLETED"
