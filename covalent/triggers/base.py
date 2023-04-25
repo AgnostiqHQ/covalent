81c0eb3fe68c8a52cab4ff1ee07806fb9c6adf17
kessler-frost <ssanand@hawk.iit.edu>
2023-04-25 16:01:11 -0400

    Fixed redispatch and trigger server address passing in base trigger

--- covalent/triggers/base.py
+++ covalent/triggers/base.py
@@ -84,13 +84,14 @@ class BaseTrigger:
         self._register(self.to_dict(), self.triggers_server_addr)
 
     @staticmethod
-    def _register(trigger_data, triggers_server_addr=None) -> None:
+    def _register(trigger_data) -> None:
         """
         Register a trigger to the Triggers server given only its dictionary format and start observing.
 
         Args:
             trigger_data: Dictionary representation of a trigger
         """
+        triggers_server_addr = trigger_data.get("triggers_server_addr")
         if triggers_server_addr is None:
             triggers_server_addr = (
                 get_config("dispatcher.address") + ":" + str(get_config("dispatcher.port"))
@@ -149,7 +150,11 @@ class BaseTrigger:
 
         from .. import redispatch
 
-        return redispatch(self.lattice_dispatch_id, self.dispatcher_addr, is_pending)()
+        return redispatch(
+            dispatch_id=self.lattice_dispatch_id,
+            dispatcher_addr=self.dispatcher_addr,
+            is_pending=is_pending,
+        )
 
     def trigger(self) -> None:
         """
