f0fe3d67567152d8e7e61e1a6741bc168d5f4f25
kessler-frost <ssanand@hawk.iit.edu>
2023-05-08 09:23:45 -0400

    added init and print stdout unit tests

--- tests/covalent_tests/cloud_resource_manager/core_test.py
+++ tests/covalent_tests/cloud_resource_manager/core_test.py
@@ -18,9 +18,14 @@
 #
 # Relief from the License may be granted by purchasing a commercial license.
 
+
+from functools import partial
+from pathlib import Path
+
 import pytest
 
 from covalent.cloud_resource_manager.core import (
+    CloudResourceManager,
     get_converted_value,
     get_executor_module,
     validate_options,
@@ -91,5 +96,58 @@ def test_validate_options(mocker):
     assert mock_defaults_model.call_count == 2
 
 
-def test_cloud_resource_manager_init():
-    pass
+@pytest.mark.parametrize(
+    "options",
+    [
+        None,
+        {"test_key": "test_value"},
+    ],
+)
+def test_cloud_resource_manager_init(mocker, options):
+    test_executor_name = "test_executor"
+    test_executor_module_path = "test_executor_module_path"
+
+    mock_validate_options = mocker.patch(
+        "covalent.cloud_resource_manager.core.validate_options",
+    )
+
+    crm = CloudResourceManager(
+        executor_name=test_executor_name,
+        executor_module_path=test_executor_module_path,
+        options=options,
+    )
+
+    assert crm.executor_name == test_executor_name
+    assert crm.executor_tf_path == str(
+        Path(test_executor_module_path).expanduser().resolve() / "assets" / "infra"
+    )
+    assert crm.executor_options == options
+
+    if options:
+        mock_validate_options.assert_called_once_with(options, test_executor_name)
+    else:
+        mock_validate_options.assert_not_called()
+
+
+def test_print_stdout(mocker):
+    test_stdout = "test_stdout".encode("utf-8")
+    test_return_code = 0
+
+    mock_process = mocker.MagicMock()
+
+    mock_process.poll.side_effect = partial(next, iter([None, test_return_code, test_return_code]))
+    mock_process.stdout.readline.side_effect = partial(next, iter([test_stdout, None]))
+
+    mock_print = mocker.patch("covalent.cloud_resource_manager.core.print")
+
+    crm = CloudResourceManager(
+        executor_name="test_executor",
+        executor_module_path="test_executor_module_path",
+    )
+
+    return_code = crm._print_stdout(mock_process)
+
+    mock_process.stdout.readline.assert_called_once()
+    mock_print.assert_called_once_with(test_stdout.decode("utf-8"))
+    assert mock_process.poll.call_count == 3
+    assert return_code == test_return_code
