1c1cdfa659f9d1fb27a375919e5e4673d68494cd
Ara Ghukasyan <ara@agnostiq.ai>
2023-06-06 23:43:15 -0400

    delete conflicting empty module

--- covalent/experimental/covalent_qelectron/executors/selectors.py
+++
@@ -1,6 +0,0 @@
-# from covalent_qelectron.executors.base import BaseQSelector
-
-
-# class SimpleSelector(BaseQSelector):
-#     def __call__(self, qscript, executors):
-#         return self.selector_function(qscript, executors)
