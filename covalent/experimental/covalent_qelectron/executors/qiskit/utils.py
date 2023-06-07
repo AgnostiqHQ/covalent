d12dd5be4628e08108817044badf50b4f9f86394
Ara Ghukasyan <ara@agnostiq.ai>
2023-06-07 17:51:11 -0400

    remove meas enum, custom exception, and conversion func

--- covalent/experimental/covalent_qelectron/executors/qiskit/utils.py
+++ covalent/experimental/covalent_qelectron/executors/qiskit/utils.py
@@ -1,26 +1,15 @@
+"""
+Utilities for Qiskit-based QElectron executors and devices
+"""
 from dataclasses import dataclass
-from enum import Enum
 from typing import Optional
 
-from qiskit.quantum_info import SparsePauliOp
 from qiskit_ibm_runtime import Options, options
-
-MEASURE_OP_NAME = "measure"
-
-
-class _Options:
-    def get(self, k, default=None):
-        """
-        mimics a `dict` object's `get` method using instance's `__dict__`,
-        defaults when the value is None
-        """
-        if k in self.__dict__ and self.__dict__[k]:
-            return self.__dict__[k]
-        return default
 
 
 @dataclass(frozen=True)
-class RuntimeOptions(_Options):
+class RuntimeOptions:
+    # pylint: disable=too-many-instance-attributes
     """
     Execution options of Qiskit Runtime
     """
@@ -33,30 +22,14 @@ class RuntimeOptions(_Options):
     environment: Optional[options.EnvironmentOptions] = None
     simulator: Optional[options.SimulatorOptions] = None
 
-
-class PennylaneQiskitError(Exception):
-    """
-    For errors related to the Pennylane-to-Qiskit interfacing code
-    """
-
-
-class Measurement(str, Enum):
-    """
-    Enumeration of Pennylane measurement types.
-    see: https://docs.pennylane.ai/en/stable/introduction/measurements.html
-    """
-    EXPVAL = "expval"
-    SAMPLE = "sample"
-    COUNTS = "counts"
-    VAR = "var"
-    PROBS = "probs"
-    STATE = "state"
-    DENSITY_MATRIX = "density_matrix"
-    VN_ENTROPY = "vn_entropy"
-    MUTUAL_INFO = "mutual_info"
-    PURITY = "purity"
-    CLASSICAL_SHADOW = "classical_shadow"
-    SHADOW_EXPVAL = "shadow_expval"
+    def get(self, k, default=None):
+        """
+        mimics a `dict` object's `get` method using instance's `__dict__`,
+        defaults when the value is None
+        """
+        if k in self.__dict__ and self.__dict__[k]:
+            return self.__dict__[k]
+        return default
 
 
 def extract_options(opts: dict) -> Options:
@@ -73,34 +46,3 @@ def extract_options(opts: dict) -> Options:
     _options.environment = opts.get("environment", _options.environment)
     _options.simulator = opts.get("simulator", _options.simulator)
     return _options
-
-
-def convert_qscripts(compiled_circuits):
-    """
-    Compile circuits and extract observables
-    """
-    observables = []
-
-    # extract observables
-    for cc in compiled_circuits:
-
-        # instructions are sorted, with measurements at the end; iterate in
-        # reverse to avoid looping through preceding instructions
-        measure_ops = []
-        for circ_instr in reversed(cc.data):
-            if circ_instr.operation.name != MEASURE_OP_NAME:
-                break
-
-            # remove measurement gate
-            measure_ops.append(cc.data.pop(-1))
-
-        # construct a Qiskit `SparsePauliOp` to represent observable
-        obs_chars = ["I"] * cc.num_qubits
-        for measure_op in reversed(measure_ops):
-            for q in measure_op.qubits:
-                obs_chars[q.index] = "Z"
-
-        obs_string = "".join(obs_chars)
-        observables.append(SparsePauliOp(obs_string))
-
-    return compiled_circuits, observables
