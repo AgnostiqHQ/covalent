a869d219ae174854fa31bcfa3177ad8ea5b95b45
sankalp <sankalp@agnostiq.ai>
2023-07-04 11:15:07 -0400

    using filelock package for platform independency in locking the config file

--- covalent/_shared_files/config.py
+++ covalent/_shared_files/config.py
@@ -18,7 +18,7 @@
 #
 # Relief from the License may be granted by purchasing a commercial license.
 
-import fcntl
+
 import os
 import shutil
 from dataclasses import asdict
@@ -28,6 +28,7 @@ from pathlib import Path
 from typing import Any, Dict, List, Optional, Union
 
 import toml
+import filelock
 
 """Configuration manager."""
 
@@ -109,8 +110,7 @@ class ConfigManager:
                         else:
                             old_dict.setdefault(key, value)
 
-        with open(self.config_file, "r+") as f:
-            fcntl.lockf(f, fcntl.LOCK_EX)
+        with open(self.config_file, "r") as f:
             file_config = toml.load(f)
 
         update_nested_dict(self.config_data, file_config)
@@ -143,9 +143,10 @@ class ConfigManager:
         Returns:
             None
         """
-        with open(self.config_file, "w") as f:
-            fcntl.lockf(f, fcntl.LOCK_EX)
-            toml.dump(self.config_data, f)
+
+        with filelock.FileLock(f"{self.config_file}.lock"):
+            with open(self.config_file, "w") as f:
+                toml.dump(self.config_data, f)
 
     def purge_config(self) -> None:
         """
