271681010492965a2f7f8e1b5765af7c8c654e5d
Ara Ghukasyan <ara@agnostiq.ai>
2023-07-14 21:27:54 -0400

    test ux via decorator

--- tests/qelectron_tests/test_decorator.py
+++ tests/qelectron_tests/test_decorator.py
@@ -20,6 +20,9 @@
 
 # pylint: disable=no-member
 
+import copy
+import random
+
 import pennylane as qml
 import pytest
 from numpy import isclose
@@ -72,3 +75,130 @@ def test_decorator_vs_explicit_wrapper(executor):
 
     assert isinstance(results[0], type(results[1])), f"Results {results!r} are not the same type"
     assert isclose(results[0], results[1], 0.1), f"Results {results!r} are not close"
+
+
+class TestDecoratorArguments:
+    """
+    Test that the `ct.qelectron` decorator accepts and correctly processes various
+    types of `executors` arguments.
+
+    Specifically, the following types should be supported and the corresponding
+    behavior observed:
+
+    (1) `executors=executor_1`
+        -> a single executor is used for all circuits
+
+    (2) `executors=[executor_1, ..., executor_N]`
+        -> a `QCluster` is created from the two or more executor instances
+
+    (3) `executors=qcluster_1`
+        -> the given `QCluster` is used for all circuits
+    """
+
+    @pytest.mark.parametrize("executor", EXECUTORS)
+    def test_single_executor(self, executor):
+        """
+        Test that the `ct.qelectron` decorator accepts a single executor (case 1).
+        """
+
+        dev = qml.device("default.qubit", wires=2)
+
+        # QElectron definition.
+        @ct.qelectron(executors=executor)
+        @qml.qnode(device=dev)
+        def qelectron_circuit(param):
+            qml.RX(param, wires=0)
+            qml.Hadamard(wires=1)
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.operation.Tensor(*(qml.PauliY(0), qml.PauliX(1))))
+
+        # Equivalent QNode definition (for comparison).
+        @qml.qnode(device=dev)
+        def normal_circuit(param):
+            qml.RX(param, wires=0)
+            qml.Hadamard(wires=1)
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.operation.Tensor(*(qml.PauliY(0), qml.PauliX(1))))
+
+        res = normal_circuit(0.5)
+        qres = qelectron_circuit(0.5)
+
+        assert isinstance(qres, type(res)), f"Results {res!r} and {qres!r} are not the same type"
+        assert isclose(qres, res, 0.1), f"Results {res!r} and {qres!r} are not close"
+
+    @pytest.mark.parametrize("executor", EXECUTORS)
+    def test_list_of_executors(self, executor):
+        """
+        Test that the `ct.qelectron` decorator accepts a list of executors (case 2).
+        """
+
+        dev = qml.device("default.qubit", wires=2)
+
+        # Create a list of executors.
+        executors_list = [executor, copy.deepcopy(executor)]
+
+        # QElectron definition.
+        @ct.qelectron(executors=executors_list)
+        @qml.qnode(device=dev)
+        def qelectron_circuit(param):
+            qml.RX(param, wires=0)
+            qml.Hadamard(wires=1)
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.operation.Tensor(*(qml.PauliY(0), qml.PauliX(1))))
+
+        # Check that the executor is a `QCluster`.
+        assert len(qelectron_circuit.device.executors) == 1
+        assert isinstance(qelectron_circuit.device.executors[0], ct.executor.QCluster)
+
+        # Equivalent QNode definition (for comparison).
+        @qml.qnode(device=dev)
+        def normal_circuit(param):
+            qml.RX(param, wires=0)
+            qml.Hadamard(wires=1)
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.operation.Tensor(*(qml.PauliY(0), qml.PauliX(1))))
+
+        res = normal_circuit(0.5)
+        qres = qelectron_circuit(0.5)
+
+        assert isinstance(qres, type(res)), f"Results {res!r} and {qres!r} are not the same type"
+        assert isclose(qres, res, 0.1), f"Results {res!r} and {qres!r} are not close"
+
+    @pytest.mark.parametrize("executor", EXECUTORS)
+    def test_explicit_qcluster(self, executor):
+        """
+        Test that the `ct.qelectron` decorator accepts a `QCluster` instance (case 3).
+        """
+
+        dev = qml.device("default.qubit", wires=2)
+
+        # Create a `QCluster` explicitly.
+        executors_list = [executor, copy.deepcopy(executor)]
+        qcluster = ct.executor.QCluster(executors=executors_list)
+
+        # QElectron definition.
+        @ct.qelectron(executors=qcluster)
+        @qml.qnode(device=dev)
+        def qelectron_circuit(param):
+            qml.RX(param, wires=0)
+            qml.Hadamard(wires=1)
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.operation.Tensor(*(qml.PauliY(0), qml.PauliX(1))))
+
+        # Check that the executor is a `QCluster`.
+        assert len(qelectron_circuit.device.executors) == 1
+        assert isinstance(qelectron_circuit.device.executors[0], ct.executor.QCluster)
+
+        # Equivalent QNode definition (for comparison).
+        @qml.qnode(device=dev)
+        def normal_circuit(param):
+            qml.RX(param, wires=0)
+            qml.Hadamard(wires=1)
+            qml.CNOT(wires=[0, 1])
+            return qml.expval(qml.operation.Tensor(*(qml.PauliY(0), qml.PauliX(1))))
+
+        res = normal_circuit(0.5)
+        qres = qelectron_circuit(0.5)
+
+        assert isinstance(qres, type(res)), f"Results {res!r} and {qres!r} are not the same type"
+        assert isclose(qres, res, 0.1), f"Results {res!r} and {qres!r} are not close"
