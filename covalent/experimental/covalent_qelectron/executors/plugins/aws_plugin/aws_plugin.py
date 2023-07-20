0089f0c00b09dde42bcbf4aed87ab4df7a9bac7c
jackbaker1001 <jsbaker1001@gmail.com>
2023-07-19 23:58:53 -0400

    removal of unused imports and change over to process pool executor for local braket as this makes more sense for local execution in python.

--- covalent/experimental/covalent_qelectron/executors/plugins/aws_plugin/aws_plugin.py
+++ covalent/experimental/covalent_qelectron/executors/plugins/aws_plugin/aws_plugin.py
@@ -30,12 +30,11 @@ from braket.aws import AwsQuantumTask, AwsQuantumTaskBatch
 
 from covalent._shared_files.config import get_config
 from covalent.experimental.covalent_qelectron.executors.base import (
-    AsyncBaseQExecutor,
+    BaseProcessPoolQExecutor,
     BaseThreadPoolQExecutor,
-    BaseQExecutor,
     QCResult,
-    get_asyncio_event_loop,
-    get_thread_pool
+    get_thread_pool, 
+    get_process_pool
 )
 from covalent.experimental.covalent_qelectron.shared_utils import import_from_path
 
@@ -48,17 +47,34 @@ _QEXECUTOR_PLUGIN_DEFAULTS = {
 
     "BraketQubitExecutor": {
         "device_arn": "",
-        "s3_destination_folder": ""
+        "s3_destination_folder": "",
+        "poll_timeout_seconds": AwsQuantumTask.DEFAULT_RESULTS_POLL_TIMEOUT,
+        "poll_interval_seconds": AwsQuantumTask.DEFAULT_RESULTS_POLL_INTERVAL,
+        "aws_session": "",
+        "parallel": False,
+        "max_parallel": None,
+        "max_connections": AwsQuantumTaskBatch.MAX_CONNECTIONS_DEFAULT,
+        "max_retries": AwsQuantumTaskBatch.MAX_RETRIES,
+        "run_kwargs": {},
+        "max_jobs": 20
     },
 
     "LocalBraketQubitExecutor": {
-        "backend": "default"
+        "backend": "default",
+        "shots": None,
+        "run_kwargs": {},
+        "max_jobs": 20
     }
 } 
 
 class BraketQubitExecutor(BaseThreadPoolQExecutor):
+    """The remote Braket executor based on the existing pennylane braket qubit device.
+    Usage of this device requires valid AWS credentials as set up following the
+    instructions at https://github.com/aws/amazon-braket-sdk-python#prerequisites 
+    """
 
     max_jobs: int = 20
+    shots: int = None,
     device_arn: str = None
     poll_timeout_seconds: float = AwsQuantumTask.DEFAULT_RESULTS_POLL_TIMEOUT
     poll_interval_seconds: float = AwsQuantumTask.DEFAULT_RESULTS_POLL_INTERVAL
@@ -69,7 +85,8 @@ class BraketQubitExecutor(BaseThreadPoolQExecutor):
     max_retries: int = AwsQuantumTaskBatch.MAX_RETRIES
     run_kwargs: dict = {}
     s3_destination_folder: str = Field(
-        default_factory=lambda: get_config("qelectron")["BraketQubitExecutor"]["s3_destination_folder"]
+        default_factory=lambda:\
+              get_config("qelectron")["BraketQubitExecutor"]["s3_destination_folder"]
     )
     def batch_submit(self, qscripts_list):
 
@@ -100,8 +117,11 @@ class BraketQubitExecutor(BaseThreadPoolQExecutor):
             jobs.append(p.submit(self.run_circuit, qscript, dev, result_obj))
 
         return jobs
-    
-class LocalBraketQubitExecutor(BaseThreadPoolQExecutor):
+
+
+class LocalBraketQubitExecutor(BaseProcessPoolQExecutor):
+    """The local Braket executor based on the existing pennylane local braket qubit device. 
+    """
     
     max_jobs: int = 20
     shots: int = None
@@ -112,8 +132,8 @@ class LocalBraketQubitExecutor(BaseThreadPoolQExecutor):
 
     def batch_submit(self, qscripts_list):
 
-        p = get_thread_pool(self.max_jobs)
-        jobs = []
+        pool = get_process_pool(self.num_processes)
+        futures = []
         for qscript in qscripts_list:
             dev = qml.device(
                 "braket.local.qubit",
@@ -128,6 +148,7 @@ class LocalBraketQubitExecutor(BaseThreadPoolQExecutor):
                 executor=self,
             )
 
-            jobs.append(p.submit(self.run_circuit, qscript, dev, result_obj))
+            fut = pool.apply_async(self.run_circuit, args=(qscript, dev, result_obj))
+            futures.append(fut)
 
-        return jobs
+        return futures
