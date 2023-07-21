e588954f348e272df641157e981d2e410d0793d2
jackbaker1001 <jsbaker1001@gmail.com>
2023-07-20 23:19:38 -0400

    Added full docstrings

--- covalent/experimental/covalent_qelectron/executors/plugins/aws_plugin/aws_plugin.py
+++ covalent/experimental/covalent_qelectron/executors/plugins/aws_plugin/aws_plugin.py
@@ -64,9 +64,32 @@ _QEXECUTOR_PLUGIN_DEFAULTS = {
 } 
 
 class BraketQubitExecutor(BaseThreadPoolQExecutor):
-    """The remote Braket executor based on the existing pennylane braket qubit device.
-    Usage of this device requires valid AWS credentials as set up following the
-    instructions at https://github.com/aws/amazon-braket-sdk-python#prerequisites 
+    """
+    The remote Braket executor based on the existing Pennylane Braket
+    qubit device. Usage of this device requires valid AWS credentials as 
+    set up following the instructions at 
+    https://github.com/aws/amazon-braket-sdk-python#prerequisites.
+
+    Attributes:
+        max_jobs: 
+            maximum number of parallel jobs sent by threads on batch_submit
+        shots: number of shots used to estimate quantum observables
+        device_arn: 
+            an alpha-numeric code (arn=Amazon Resource Name) specifying a quantum device
+        poll_timeout_seconds: 
+            number of seconds before a poll to remote device is considered timed-out
+        poll_interval_seconds: 
+            number of seconds between polling of a remote device's status
+        aws_session: 
+            an An AwsSession object created to manage interactions with AWS services, 
+            to be supplied if extra control is desired. 
+        parallel: turn parallel execution on or off
+        max_parallel: the maximum number of circuits to be executed in parallel
+        max_connections: the maximum number of connections in the Boto3 connection pool.
+        max_retries: the maximum number of time a job will be re-sent if it failed
+        s3_destination_folder: Name of the S3 bucket and folder, specified as a tuple.
+        run_kwargs: Variable length keyword arguments for ``braket.devices.Device.run()``
+
     """
 
     max_jobs: int = 20
@@ -79,12 +102,22 @@ class BraketQubitExecutor(BaseThreadPoolQExecutor):
     max_parallel: Optional[int] = None
     max_connections: int = AwsQuantumTaskBatch.MAX_CONNECTIONS_DEFAULT
     max_retries: int = AwsQuantumTaskBatch.MAX_RETRIES
-    run_kwargs: dict = {}
-    s3_destination_folder: str = Field(
+    s3_destination_folder: tuple = Field(
         default_factory=lambda:\
               get_config("qelectron")["BraketQubitExecutor"]["s3_destination_folder"]
     )
+    run_kwargs: dict = {}
+
     def batch_submit(self, qscripts_list):
+        """
+        Submit qscripts for execution using max_jobs-many threads.
+
+        Args:
+            qscripts_list: a list of Pennylane style QuantumScripts
+
+        Returns:
+            jobs: a list of tasks subitted by threads.
+        """
 
         p = get_thread_pool(self.max_jobs)
         jobs = []
@@ -116,17 +149,35 @@ class BraketQubitExecutor(BaseThreadPoolQExecutor):
 
 
 class LocalBraketQubitExecutor(BaseProcessPoolQExecutor):
-    """The local Braket executor based on the existing pennylane local braket qubit device. 
+    """
+    The local Braket executor based on the existing Pennylane local Braket qubit device.
+
+    Attributes:
+        max_jobs: maximum number of parallel jobs sent by proccesses on batch_submit
+        shots: number of shots used to estimate quantum observables
+        backend: 
+            The name of the simulator backend. Defaults to the ``default`` 
+            simulator backend name.
+        run_kwargs: Variable length keyword arguments for ``braket.devices.Device.run()``
     """
     
     max_jobs: int = 20
     shots: int = None
-    run_kwargs: dict = {}
     backend: str = Field(
         default_factory=lambda: get_config("qelectron")["LocalBraketQubitExecutor"]["backend"]
     )
+    run_kwargs: dict = {}
 
     def batch_submit(self, qscripts_list):
+        """
+        Submit qscripts for execution using num_processes-many processes.
+
+        Args:
+            qscripts_list: a list of Pennylane style QuantumScripts
+
+        Returns:
+            jobs: a list of futures subitted by processes
+        """
 
         pool = get_process_pool(self.num_processes)
         futures = []
