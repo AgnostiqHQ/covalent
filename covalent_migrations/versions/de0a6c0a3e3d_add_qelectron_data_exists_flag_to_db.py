0dd2f42b45a890be9b76dee81f6a168c0acb0eb9
sankalp <sankalp@agnostiq.ai>
2023-09-19 08:07:37 -0400

    fixing tests

--- covalent_migrations/versions/de0a6c0a3e3d_add_qelectron_data_exists_flag_to_db.py
+++ covalent_migrations/versions/de0a6c0a3e3d_add_qelectron_data_exists_flag_to_db.py
@@ -44,7 +44,9 @@ def upgrade() -> None:
 
     with op.batch_alter_table("electrons", schema=None) as batch_op:
         batch_op.add_column(
-            sa.Column("qelectron_data_exists", sa.Boolean(), nullable=False, default=False)
+            sa.Column(
+                "qelectron_data_exists", sa.Boolean(), nullable=False, server_default=sa.false()
+            )
         )
 
     # ### end Alembic commands ###
