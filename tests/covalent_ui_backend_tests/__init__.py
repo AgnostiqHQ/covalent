e97e9c67d949df01fdaafbba70d4576c48c430d2
Will Cunningham <wjcunningham7@gmail.com>
2023-09-22 19:56:45 -0400

    fixing tests

--- tests/covalent_ui_backend_tests/__init__.py
+++ tests/covalent_ui_backend_tests/__init__.py
@@ -14,11 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-from enum import Enum
-
-
-class FileName(Enum):
-    """File names"""
-
-    RESULTS = "result"
+from covalent_ui.app import fastapi_app
