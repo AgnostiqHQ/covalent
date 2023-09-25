44061cb8c7092cb5f7f76e250939ef53ede5fda7
ArunPsiog <arun.mukesh@psiog.com>
2023-09-25 21:02:30 +0530

    Changes for triggers rtd and test

---
+++ tests/covalent_tests/triggers/database_trigger_test.py
@@ -0,0 +1,121 @@
+# Copyright 2023 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+
+import pytest
+
+from covalent.triggers.database_trigger import DatabaseTrigger
+
+
+@pytest.fixture
+def database_trigger():
+    """
+    Fixture to obtain a sample Database Trigger instance for testing
+    """
+
+    db_path = "test_db_path"
+    table_name = "test_table_name"
+    poll_interval = 1
+    where_clauses = ["id > 2", "status = pending"]
+    trigger_after_n = 1
+    return DatabaseTrigger(db_path, table_name, poll_interval, where_clauses, trigger_after_n)
+
+
+def test_sqlite_trigger_init(database_trigger):
+    """
+    Testing whether Database Trigger initializes as expected
+    """
+
+    assert database_trigger.db_path == "test_db_path"
+    assert database_trigger.table_name == "test_table_name"
+    assert database_trigger.poll_interval == 1
+    assert database_trigger.where_clauses == ["id > 2", "status = pending"]
+    assert database_trigger.trigger_after_n == 1
+
+
+@pytest.mark.parametrize(
+    "where_clauses",
+    [
+        ["id > 2", "status = pending"],
+        None,
+    ],
+)
+def test_database_trigger_observe(mocker, where_clauses, database_trigger):
+    """
+    Test the observe method of Database Trigger
+    """
+
+    database_trigger.where_clauses = where_clauses
+    database_trigger.trigger = mocker.MagicMock()
+
+    mock_db_engine = mocker.patch("covalent.triggers.database_trigger.create_engine")
+    mock_session = mocker.patch("covalent.triggers.database_trigger.Session")
+    mock_event = mocker.patch("covalent.triggers.database_trigger.Event")
+    mock_sleep = mocker.patch("covalent.triggers.database_trigger.time.sleep")
+
+    mock_event.return_value.is_set.side_effect = [False, True]
+    database_trigger.observe()
+
+    sql_poll_cmd = "SELECT * FROM test_table_name"
+    if where_clauses:
+        sql_poll_cmd += " WHERE "
+        sql_poll_cmd += " AND ".join(list(where_clauses))
+    sql_poll_cmd += ";"
+
+    mock_db_engine.assert_called_once_with("test_db_path")
+    mock_session.assert_called_once_with(mock_db_engine("test_db_path"))
+    mock_event.assert_called_once()
+    mock_sql_execute = mocker.patch.object(mock_session, "execute", autospec=True)
+    mock_sql_execute.assert_called_once_with(sql_poll_cmd)
+    mock_sleep.assert_called_once_with(1)
+
+
+# def test_database_trigger_exception(mocker, database_trigger):
+#     """
+#     Test the observe method of Database trigger when an OperationalError is raised
+#     """
+
+#     database_trigger.trigger = mocker.MagicMock()
+#     mock_event = mocker.patch("covalent.triggers.database_trigger.Event")
+#     mock_sleep = mocker.patch("covalent.triggers.database_trigger.time.sleep")
+#     mock_event.return_value.is_set.side_effect = [False, True]
+#     mock_session = mocker.patch('covalent.triggers.database_trigger.Session')
+#     import sqlalchemy
+#     from sqlalchemy.exc import ArgumentError
+#     mock_session.side_effect = ArgumentError
+
+#     # Call the 'observer' method
+#     try:
+#         database_trigger.observe()
+#     except sqlalchemy.exc.ArgumentError as exc:
+#         assert str(exc) == "Could not parse SQLAlchemy URL from string 'test_db_path'"
+
+
+def test_database_trigger_stop(mocker, database_trigger):
+    """
+    Test the stop method of Database Trigger
+    """
+
+    mock_stop_flag = mocker.MagicMock()
+    database_trigger.stop_flag = mock_stop_flag
+
+    database_trigger.stop()
+
+    mock_stop_flag.set.assert_called_once()
