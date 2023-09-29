4ad1d24c23a5d945ba48e8fb4dbbd973501ea411
Andrew S. Rosen <asrosen93@gmail.com>
2023-09-28 17:13:02 -1000

    Update utils.py

--- covalent/_shared_files/utils.py
+++ covalent/_shared_files/utils.py
@@ -20,14 +20,16 @@ import importlib
 import inspect
 import socket
 from datetime import timedelta
-from typing import Any, Callable, Dict, Set, Tuple
+from typing import Any, Callable, Dict, Set, Tuple, TYPE_CHECKING
 
 import cloudpickle
-from pennylane._device import Device
 
 from . import logger
 from .config import get_config
 from .pickling import _qml_mods_pickle
+
+if TYPE_CHECKING:
+    from pennylane._device import Device
 
 app_log = logger.app_log
 log_stack_info = logger.log_stack_info
