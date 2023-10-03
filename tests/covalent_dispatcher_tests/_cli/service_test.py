0745b19b8445fd6f18956b7fc8cf27df021e105f
sankalp <sankalp@agnostiq.ai>
2023-10-03 19:22:08 -0400

    fixing tests

--- tests/covalent_dispatcher_tests/_cli/service_test.py
+++ tests/covalent_dispatcher_tests/_cli/service_test.py
@@ -596,7 +596,7 @@ def test_logs(exists, mocker):
     if not exists:
         result = runner.invoke(logs)
         assert (
-            result.output.replace("\n", " ").strip()
+            result.output.replace("\n", "")
             == f"{UI_LOGFILE} not found. Restart the server to create a new log file."
         )
     else:
