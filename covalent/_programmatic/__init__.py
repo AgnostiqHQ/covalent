46b395c8600281524de8dc00bed3602394a01af8
Ara Ghukasyan <ara@agnostiq.ai>
2023-11-20 15:40:55 -0500

    move commands to main namespace

--- covalent/_programmatic/__init__.py
+++ covalent/_programmatic/__init__.py
@@ -13,5 +13,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from .entry_point import cancel_running_dispatch, run_dispatcher
--- covalent/_programmatic/commands.py
+++ covalent/_programmatic/commands.py
@@ -1,3 +1,19 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the Apache License 2.0 (the "License"). A copy of the
+# License may be obtained with this software package or at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Use of this file is prohibited except in compliance with the License.
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Functions providing programmatic access to Covalent CLI commands."""
 import time
 from typing import Any, Dict, Optional
@@ -5,10 +21,10 @@ from typing import Any, Dict, Optional
 import click
 import psutil
 
-from covalent import get_config
-from covalent._shared_files import logger
+from covalent_dispatcher._cli.service import _read_pid, start, stop
 
-from ._cli.service import _read_pid, start, stop
+from .._shared_files import logger
+from .._shared_files.config import get_config
 
 app_log = logger.app_log
 
