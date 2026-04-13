ac626f75de944fa3ecc3e10adf9199048be16fa7
Ara Ghukasyan <ara@agnostiq.ai>
2026-04-13 13:11:18 -0400

    fix tests

--- tests/covalent_dispatcher_tests/_core/data_modules/job_manager_test.py
+++ tests/covalent_dispatcher_tests/_core/data_modules/job_manager_test.py
@@ -100,6 +100,7 @@ async def test_set_job_handle(mocker):
     mock_update.assert_called_with([{"job_id": 1, "job_handle": "12356"}])
 
 
+@pytest.mark.asyncio
 async def test_set_job_status(mocker):
     """
     Test requesting a task to be cancelled
