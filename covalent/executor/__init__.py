a900c9a8d72f46f6bc314b82898ac77f70ea4992
Ara Ghukasyan <ara@agnostiq.ai>
2026-04-13 10:12:45 -0400

    ensure executor module load fails silently

--- covalent/executor/__init__.py
+++ covalent/executor/__init__.py
@@ -244,10 +244,7 @@ class _ExecutorManager:
                     the_module = importlib.util.module_from_spec(module_spec)
                     try:
                         module_spec.loader.exec_module(the_module)
-                    except ImportError as e:
-                        app_log.warning(
-                            "Skipping executor plugin %s: missing dependency (%s)", module_file, e
-                        )
+                    except ImportError:
                         continue
 
                     self._populate_executor_map_from_module(the_module)
