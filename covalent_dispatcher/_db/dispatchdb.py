b02cb5935d53c85de3b0c1e752a2b97c01f78278
Ara Ghukasyan <ara@agnostiq.ai>
2026-04-14 11:19:01 -0400

    restore 'links' key in extract_graph for 'json_graph.node_link_data'

--- covalent_dispatcher/_db/dispatchdb.py
+++ covalent_dispatcher/_db/dispatchdb.py
@@ -112,7 +112,7 @@ def extract_graph(graph):
     nodes = list(map(extract_graph_node, graph["nodes"]))
     return {
         "nodes": nodes,
-        "edges": graph["edges"],
+        "edges": graph["links"],
     }
 
 
