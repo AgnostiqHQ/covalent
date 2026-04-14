0fc7bf645994ea0b77c4bba438248bc89165bae3
Ara Ghukasyan <ara@agnostiq.ai>
2026-04-13 20:25:43 -0400

    add another ignore_files entry in docs how to test

--- tests/functional_tests/docs_how_to_test.py
+++ tests/functional_tests/docs_how_to_test.py
@@ -42,6 +42,7 @@ ignore_files = [
     "creating_custom_executors.ipynb",
     "file_transfers_to_from_azure_blob.ipynb",
     "file_transfers_to_from_gcp_storage.ipynb",
+    "file_transfers_to_from_s3.ipynb",
     "file_transfers_for_workflows_to_remote.ipynb",
     "file_transfers_to_remote.ipynb",
     "trigger_sqlite.ipynb",  # TODO: Temp skip, see issue #1808
